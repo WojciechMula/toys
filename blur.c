4a92c80aaac524c28ca5c001aa9533d9cfbbf980
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-07-14 16:24:43 +0000

    * new functions:   * blur_gray_calc_avg   * x86blur_calc_gray_sums   * x86blur_calc_gray_avg   * mmxblur_calc_gray_sums   * mmxblur_calc_gray_avg * macro `define_blur_gray_img_fun' * X help * allow to change on-line blur routines
    git-svn-id: file:///mnt/data/SVNrepo@1122 a13746d7-5020-4d46-8679-245a07f888a7

--- blur.c
+++ blur.c
@@ -2,8 +2,29 @@
 #include <stdint.h>
 #include <string.h>
 
-void blur_calc_gray_sums(
+#define SUM_TABLE_SIZE		(1024*16)
+#define MAX_GRAY_WIDTH		SUM_TABLE_SIZE
+#define MAX_32bpp_WIDTH		(SUM_TABLE_SIZE/4)
+
+uint16_t SUM_TABLE[3*SUM_TABLE_SIZE];
+
+uint8_t divide_lookup[255*9 + 1]; // sum \in [0..9*255]
+
+void initialize_divide_lookup() {
+	unsigned int j, i;
+	for (i=0; i < sizeof(divide_lookup); i++)
+		divide_lookup[i] = (i * (65536/9) + (65536/9)) >> 16;
+}
+
+#define inlinefun __attribute__ ((always_inline))
+
+
+// C-language implementation
+
+void inlinefun
+blur_gray_calc_sums(
 	uint8_t  *src_img,
+	uint8_t  border_color,
 	uint16_t *sum_tbl,
 	unsigned int width
 ) {
@@ -12,26 +33,50 @@ void blur_calc_gray_sums(
 	uint16_t a, b, c;
 
 	pix = src_img;
-	a = 0x00;//*pix++;
-	b = pix[0];//*pix++;
+	a = border_color;
+	b = *pix++;
 	for (x=0; x < width-1; x++) {
-		c = pix[x+1];//*pix++;
+		c = *pix++;
 		sum_tbl[x] = a + b + c;
 		a = b;
 		b = c;
 	}
 
-	c = 0x00;
+	c = border_color;
 	sum_tbl[width-1] = a + b + c;
 }
 
-void x86blur_calc_gray_sums(
+
+void inlinefun
+blur_gray_calc_avg(uint8_t *dst_img, unsigned int width) {
+	unsigned int x;
+	uint8_t *pix;
+	uint16_t *sum;
+
+	pix = dst_img;
+	sum = SUM_TABLE;
+	for (x=0; x < width; x++)
+		*pix++ = divide_lookup[
+			SUM_TABLE[x + 0*SUM_TABLE_SIZE] +
+			SUM_TABLE[x + 1*SUM_TABLE_SIZE] +
+			SUM_TABLE[x + 2*SUM_TABLE_SIZE]
+		];
+}
+
+
+
+// x86 implementation
+
+void //inlinefun
+x86blur_gray_calc_sums(
 	uint8_t  *src_img,
+	uint8_t   border_color,
 	uint16_t *sum_tbl,
 	unsigned int width
 ) {
-	asm(
-	"   xorl %%eax, %%eax                     \n\t" // a = 0x00
+	asm (
+	    // eax = border_color
+	"   pushl %%eax                           \n\t"
 	"   xorl %%ebx, %%ebx                     \n\t"
 	"   xorl %%ecx, %%ecx                     \n\t"
 	"                                         \n\t"
@@ -51,15 +96,52 @@ void x86blur_calc_gray_sums(
 	"   addl $2, %%edi                        \n\t" // x++;
 	"   subl $1, %%edx                        \n\t"
 	"   jnz  0b                               \n\t"
+	"                                         \n\t"
+	"   popl %%ecx                            \n\t"
+	"   add  %%ebx, %%eax                     \n\t"
+	"   add  %%ecx, %%eax                     \n\t"
+	"                                         \n\t"
+	"   movw %%ax, (%%edi)                    \n\t"
 	
 	: /* no ouput */
-	: "S" (src_img), "D" (sum_tbl), "d" (width)
-	: "%eax", "%ebx", "%ecx", "memory"
+	: "S" (src_img), "D" (sum_tbl), "d" (width-1), "a" (border_color)
+	: "%ebx", "%ecx", "memory"
 	);
 }
 
-void mmxblur_calc_gray_sums(
+void //inlinefun
+x86blur_gray_calc_avg(uint8_t *dst_img, unsigned int width) {
+	asm volatile (
+	"0:                                       \n\t"
+	"   xorl %%eax, %%eax                     \n\t"
+	"   movw    (%%esi), %%ax                 \n\t" // ax  = SUM_TABLE[i]
+	"   addw %c0(%%esi), %%ax                 \n\t" // ax += SUM_TABLE[i + SUM_TABLE_SIZE]
+	"   addw %c1(%%esi), %%ax                 \n\t"	// ax += SUM_TABLE[i + 2*SUM_TABLE_SIZE]
+	
+	"   movb %c2(%%eax), %%al                 \n\t" // bl = divide_lookup[ax]
+	"   movb %%al, (%%edi)                    \n\t" // dst_img <- bl
+	"                                         \n\t"
+	"   addl $2, %%esi                        \n\t"
+	"   addl $1, %%edi                        \n\t"
+	"                                         \n\t"
+	"   decl %%edx                            \n\t"
+	"   jnz 0b                                \n\t"
+	"                                         \n\t"
+	: /* no output */
+	: "e" (2*SUM_TABLE_SIZE), // SUM_TABLE + SUM_TABLE_SIZE
+	  "e" (4*SUM_TABLE_SIZE), // SUM_TABLE + 2*SUM_TABLE_SIZE
+	  "e" (divide_lookup),    // divtable address
+	  "S" (SUM_TABLE), "D" (dst_img), "d" (width)
+	: "%eax", "memory"
+	);
+}
+
+
+// MMX implementation
+
+void mmxblur_gray_calc_sums(
 	uint8_t  *src_img,
+	uint8_t   border_color,
 	uint16_t *sum_tbl,
 	unsigned int width
 ) {
@@ -98,151 +180,149 @@ void mmxblur_calc_gray_sums(
 	"   jnz  0b                               \n\t"
 	"                                         \n\t"
 	: /* no ouput */
-	: "S" (src_img), "D" (sum_tbl), "c" (width/8)
+	: "S" (src_img), "D" (sum_tbl+1), "c" (width/8)
+	: "memory"
+	);
+
+	// fix sum for first pixel:
+	sum_tbl[0]  = sum_tbl[1];
+	sum_tbl[0] -= (uint16_t)src_img[0];
+	sum_tbl[0] += (uint16_t)border_color;
+	
+	// fix sum for last pixel:
+	sum_tbl[width] -= (uint16_t)src_img[width];
+	sum_tbl[width] += (uint16_t)border_color;
+}
+
+
+void mmxblur_gray_calc_avg(uint8_t *dst_img, unsigned int width) {
+	static uint16_t mul_const[4] = {65536/9, 65536/9, 65536/9, 65536/9};
+	asm(
+	"  movq (%%eax), %%mm7                    \n\t"
+	"0:                                       \n\t"
+	"  movq     (%%esi), %%mm0                \n\t"
+	"  paddw %c0(%%esi), %%mm0                \n\t"
+	"  paddw %c1(%%esi), %%mm0                \n\t"
+	"                                         \n\t"
+	"  movq      8(%%esi), %%mm1              \n\t"
+	"  paddw %c0+8(%%esi), %%mm1              \n\t"
+	"  paddw %c1+8(%%esi), %%mm1              \n\t"
+	"                                         \n\t"
+	"  pmulhw %%mm7, %%mm0                    \n\t"
+	"  pmulhw %%mm7, %%mm1                    \n\t"
+	"  packuswb %%mm1, %%mm0                  \n\t"
+	"  movq %%mm0, (%%edi)                    \n\t"
+	"                                         \n\t"
+	"  addl $16, %%esi                        \n\t"
+	"  addl $8,  %%edi                        \n\t"
+	"                                         \n\t"
+	"  dec %%ecx                              \n\t"
+	"  jnz 0b                                 \n\t"
+	"                                         \n\t"
+	: /* no output */
+	: "e" (2*SUM_TABLE_SIZE), // SUM_TABLE + SUM_TABLE_SIZE
+	  "e" (4*SUM_TABLE_SIZE), // SUM_TABLE + 2*SUM_TABLE_SIZE
+	  "S" (SUM_TABLE),
+	  "D" (dst_img),
+	  "a" (mul_const),
+	  "c" (width/8)
 	: "memory"
 	);
 }
 
 
-void blur_gray_img(
-	uint8_t *src_img,
-	uint8_t *dst_img,
-	unsigned int width,
-	unsigned int height
-) {
-	static uint16_t sums[3*4096]; // should be large enough
-	
-	int x, y;
-	int base = 8192;
-
-	memset((void*)&sums[0], 0x00, width*2);
-	mmxblur_calc_gray_sums(src_img, &sums[4094], width);
-	for (y=0; y < height-1; y++) {
-		mmxblur_calc_gray_sums(src_img + (y+1)*width, &sums[base], width);
-		asm(
-		"                                         \n\t"
-		".L1:                                     \n\t"
-		"    xorl  %%eax, %%eax                   \n\t"
-		"    movw      (%%esi), %%ax              \n\t"
-		"    addw  8192(%%esi), %%ax              \n\t"
-		"    addw 16384(%%esi), %%ax              \n\t"
-		"                                         \n\t"
-		"    addl  $2, %%esi                      \n\t"
-		"    imull $7281, %%eax, %%eax            \n\t"
-		"    addl  $1, %%edi                      \n\t"
-		"    shrl  $16, %%eax                     \n\t"
-		"                                         \n\t"
-		"    decl  %%ecx                          \n\t"
-		"    movb  %%al, (%%edi)                  \n\t"
-		"                                         \n\t"
-		"    jnz   .L1                            \n\t"
-		"                                         \n\t"
-		: /* no output */
-		: "S" (sums), "D" (&dst_img[y*width-1]), "c" (width)
-		: "%eax", "%edx"
-		);
-		//for (x=0; x < width; x++)
-		//	dst_img[y*width + x] = (7281*(sums[x] + sums[4096+x] + sums[8192+x]) >> 16);
-
-		base += 4096;
-		if (base == 12288)
-			base = 0;
-	}
+// scheme
+#define define_blur_gray_img_fun(fun_name, sum_fun_name, avg_fun_name)\
+void fun_name(													\
+	uint8_t *src_img,											\
+	uint8_t *dst_img,											\
+	unsigned int width,											\
+	unsigned int height,										\
+	uint8_t border_color										\
+) {																\
+	int x, y, i;												\
+	int offset = 2*SUM_TABLE_SIZE;								\
+																\
+	for (i=0; i < width; i++)									\
+		SUM_TABLE[i] = border_color*3;							\
+																\
+	sum_fun_name(												\
+		src_img,												\
+		border_color,											\
+		&SUM_TABLE[SUM_TABLE_SIZE],								\
+		width													\
+	);															\
+																\
+	for (y=0; y < height-1; y++) {								\
+		sum_fun_name(											\
+			src_img + (y+1)*width,								\
+			border_color,										\
+			&SUM_TABLE[offset],									\
+			width												\
+		);														\
+		avg_fun_name(											\
+			dst_img + (y*width),								\
+			width												\
+		);														\
+		offset += SUM_TABLE_SIZE;								\
+		if (offset == 3*SUM_TABLE_SIZE)							\
+			offset = 0;											\
+	}															\
+																\
+	for (i=0; i < width; i++)									\
+		SUM_TABLE[i] = border_color*3;							\
+																\
+	avg_fun_name(												\
+		dst_img + (y*width),									\
+		width													\
+	);															\
 }
 
 
-void blur_calc_32bpp_sums(
-	uint8_t  *src_img,
-	uint16_t *sum_tbl,
-	unsigned int width
-) {
-	int x;
-	uint8_t *pix;
-	uint16_t Ra, Rb, Rc;
-	uint16_t Ga, Gb, Gc;
-	uint16_t Ba, Bb, Bc;
-
-	pix = src_img;
-/*	Ra = *pix++;
-	Ga = *pix++;
-	Ba = *pix++;
-	pix++;*/
-
-	Ra = Ga = Ba = 0x00;
-	
-	Rb = *pix++;
-	Gb = *pix++;
-	Bb = *pix++;
-	pix++;
-
-	for (x=0; x < width-1; x++) {
-		Rc = *pix++;
-		Gc = *pix++;
-		Bc = *pix++;
-		pix++;
-
-		sum_tbl[3*x + 0] = Ra + Rb + Rc;
-		sum_tbl[3*x + 1] = Ga + Gb + Gc;
-		sum_tbl[3*x + 2] = Ba + Bb + Bc;
-		Ra = Rb;
-		Rb = Rc;
-		
-		Ga = Gb;
-		Gb = Gc;
-		
-		Ba = Bb;
-		Bb = Bc;
-	}
-	
-	Rc = Gc = Bc = 0x00;
-	sum_tbl[3*(width-1) + 0] = Ra + Rb + Rc;
-	sum_tbl[3*(width-1) + 1] = Ga + Gb + Gc;
-	sum_tbl[3*(width-1) + 2] = Ba + Bb + Bc;
-}
+define_blur_gray_img_fun(
+	blur_gray_img,
+	blur_gray_calc_sums,
+	blur_gray_calc_avg
+)
 
 
-void blur_32bpp_img(
-	uint8_t *src_img,
-	uint8_t *dst_img,
-	unsigned int width,
-	unsigned int height
-) {
-	static uint16_t sum_0[4096]; // should be large enough
-	static uint16_t sum_1[4096]; // for present screens
-	static uint16_t sum_2[4096];
-
-	uint16_t *A, *B, *C, *T;
+define_blur_gray_img_fun(
+	x86blur_gray_img,
+	x86blur_gray_calc_sums,
+	x86blur_gray_calc_avg
+)
 
 
-	int x, y;
-
-	A = &sum_0[0];
-	B = &sum_1[0];
-	C = &sum_2[0];
-
-	memset((void*)A, 0x00, width*2*3);
-	blur_calc_32bpp_sums(src_img, B, width);
-	for (y=0; y < height-1; y++) {
-		blur_calc_32bpp_sums(src_img + (y+1)*width*4, C, width);
-		for (x=0; x < width; x++) {
-			dst_img[y*width*4 + 4*x + 0] = (1820*(A[3*x + 0] + B[3*x + 0] + C[3*x + 0]) >> 14);
-			dst_img[y*width*4 + 4*x + 1] = (1820*(A[3*x + 1] + B[3*x + 1] + C[3*x + 1]) >> 14);
-			dst_img[y*width*4 + 4*x + 2] = (1820*(A[3*x + 2] + B[3*x + 2] + C[3*x + 2]) >> 14);
-		}
-		T = A;
-		A = B;
-		B = C;
-		C = T;
-	}
-}
+define_blur_gray_img_fun(
+	mmxblur_gray_img,
+	mmxblur_gray_calc_sums,
+	mmxblur_gray_calc_avg
+)
 
 #include <stdio.h>
 #include <string.h>
 #include "Xscr.h"
 #include "load_ppm.h"
 
+typedef enum {C_lang, x86, MMX} Function;
+
+Function current = C_lang;
+uint8_t gray_border_color = 0x00;
+
 uint8_t *data;
 uint8_t *img;
+
+void Xhelp() {
+	puts(
+"b/space - blur image using current blur routine\n"
+"1 - select routine 1 (C languge reference implementation)\n"
+"2 - select routine 2 (assembler x86)\n"
+"3 - select routine 2 (assembler MMX)\n"
+"r - revert image\n"
+"\n"
+"q - quit\n"
+	);
+}
 
 void keyboard(int x, int y, Time t, KeySym c, KeyOrButtonState s, unsigned int kb_mask) {
 	if (s == Released) return;
@@ -256,18 +336,43 @@ void keyboard(int x, int y, Time t, KeySym c, KeyOrButtonState s, unsigned int k
 
 		case XK_r:
 		case XK_R:
-			memcpy( (void*)data, (void*)img, 640*480 );
+			memcpy( (void*)data, (void*)img, 640*480);
 			Xscr_redraw();
 			break;
 
+		case XK_1:
+			current = C_lang;
+			break;
+		
+		case XK_2:
+			current = x86;
+			break;
+		
+		case XK_3:
+			current = MMX;
+			break;
+
+		case XK_space:
 		case XK_b:
 		case XK_B:
-			blur_gray_img(data, data, 640, 480);
-			//blur_32bpp_img(data, data, 640, 480);
+			switch (current) {
+				case C_lang:
+					blur_gray_img(data, data, 640, 480, gray_border_color);
+					break;
+				case x86:
+					x86blur_gray_img(data, data, 640, 480, gray_border_color);
+					break;
+				case MMX:
+					mmxblur_gray_img(data, data, 640, 480, gray_border_color);
+					break;
+			}
+				     
 			Xscr_redraw();
 			break;
 	}
 }
+
+
 
 int main(int argc, char* argv[]) {
 	FILE* f;
@@ -288,12 +393,22 @@ int main(int argc, char* argv[]) {
 		printf("PPM error: %s\n", PPM_errormsg[-result]);
 		return 1;
 	}
-
+	if (width != 640 || height != 480) {
+		printf("Image 640x480 required");
+		return 1;
+	}
 	img = (uint8_t*)malloc(640*480);
+	if (!img) {
+		printf("Can't allocate memory\n");
+		return 1;
+	}
 	memcpy((void*)img, (void*)data, 640*480);
 
-	result = Xscr_mainloop(640, 480, DEPTH_gray, False,data, 
-		keyboard, NULL, NULL, "Blur demo");
+	initialize_divide_lookup();
+
+	Xhelp();
+	result = Xscr_mainloop(640, 480, DEPTH_gray, False, data,
+		keyboard, NULL, NULL, "Blur grayscale images demo");
 	if (result < 0) {
 		printf("Xscr error: %s\n", Xscr_errormsg[-result]);
 		return 1;
@@ -301,3 +416,7 @@ int main(int argc, char* argv[]) {
 
 	return 0;
 }
+
+/*
+vim: ts=4 sw=4 nowrap noexpandtab
+*/
