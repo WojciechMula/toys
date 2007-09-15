0fcaea20dd44e673c371f8a51be64bbef0d84f37
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2007-09-15 14:03:26 +0000

    * use 2 unpck[lh]ps instead of shuftps/movhlps
    git-svn-id: file:///mnt/data/SVNrepo@1370 a13746d7-5020-4d46-8679-245a07f888a7

--- sse-dotprod4.c
+++ sse-dotprod4.c
@@ -1,5 +1,5 @@
 /*
-	Calculating 4 dot products using SSE instructions, $Revision: 1.2 $
+	Calculating 4 dot products using SSE instructions, $Revision: 1.3 $
 	
 	Author: Wojciech Mu³a
 	e-mail: wojciech_mula@poczta.onet.pl
@@ -7,7 +7,7 @@
 	
 	License: public domain
 	
-	initial release 11.02.2002, last update $Date: 2007-09-13 18:56:07 $
+	initial release 11.02.2002, last update $Date: 2007-09-15 14:03:26 $
 */
 
 #include "sse-aux.inc"
@@ -35,23 +35,20 @@ asm(
    // (xmm4-xmm7 are free)
    
    // perfom additions
-   "movaps  %%xmm0, %%xmm4                 \n"
-   "shufps  $0b01000100, %%xmm1, %%xmm0    \n" // xmm0 := |B1|B0|A1|A0|
-   "movhlps %%xmm4, %%xmm1                 \n" // xmm1 := |B3|B2|A3|A2|
+   "movaps   %%xmm0, %%xmm4                \n"
+   "unpcklps %%xmm1, %%xmm0                \n" // xmm0 := |B1|A1|B0|A0|
+   "unpckhps %%xmm1, %%xmm4                \n" // xmm4 := |B3|A3|B2|A2| (xmm1 is free)
+   "movaps   %%xmm2, %%xmm1                \n"
+   "unpcklps %%xmm3, %%xmm2                \n" // xmm2 := |D1|C1|D0|C0|
+   "unpckhps %%xmm3, %%xmm1                \n" // xmm1 := |D3|C3|D2|C2| (xmm3 is free)
+   "addps    %%xmm4, %%xmm0                \n" // xmm0 := | B13 | A13 | B02 | A02 | (xmm1 is free)
+   "addps    %%xmm1, %%xmm2                \n" // xmm2 := | D13 | C13 | D02 | C02 | (xmm3 is free)
    
-   "movaps  %%xmm2, %%xmm4                 \n"
-   "shufps  $0b01000100, %%xmm3, %%xmm2    \n" // xmm2 := |D1|D0|C1|C0|
-   "movhlps %%xmm4, %%xmm3                 \n" // xmm3 := |D3|D2|C3|C2|
-   
-   "addps   %%xmm1, %%xmm0                 \n" // xmm0 := | B13 | B02 | A13 | A02 |
-   "addps   %%xmm3, %%xmm2                 \n" // xmm2 := | D13 | D02 | C13 | C02 |
-   
-   "movaps  %%xmm0, %%xmm1                 \n"
-   "shufps $0b10001000, %%xmm2, %%xmm0     \n" // xmm0 := | D02 | C02 | B02 | A02 |
-   "shufps $0b11011101, %%xmm2, %%xmm1     \n" // xmm1 := | D13 | C13 | B13 | A13 |
-   
-   "addps   %%xmm1, %%xmm0                 \n" // xmm0 := |D0123|C0123|B0123|A0123|
-   "movups  %%xmm0, (%2)                   \n"
+   "movaps   %%xmm0, %%xmm1                \n"
+   "shufps   $0b01000100, %%xmm2, %%xmm0   \n" // xmm0 := | D02 | C02 | B02 | A02 |
+   "shufps   $0b11101110, %%xmm2, %%xmm1   \n" // xmm1 := | D13 | C13 | B13 | A13 |
+   "addps    %%xmm1, %%xmm0                \n" // xmm0 := |D0123|C0123|B0123|A0123|
+   "movups   %%xmm0, (%2)                  \n"
    :
    : "r" (v1x4), "r" (v2x4), "r" (results)
    );
@@ -91,11 +88,11 @@ int main() {
 	dot4(v1, v2, results1);
 
 	printf("    ref = "); print_vect(results1);
-	printf("    sse = "); print_vect(results2);
+	printf("    SSE = "); print_vect(results2);
 	for (i=0; i<4; i++)
 		err[i] = 100.0 * (1.0 - results1[i]/results2[i]);
 	printf("err [%%] = "); print_vect(err);
 	return 0;
 }
 
-// vim: ts=4 sw=4 nowrap
+// vim: ts=8 nowrap
