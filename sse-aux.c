c21c8411ccbc46e1220eb11078893ae4d374d7ac
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2008-05-27 14:27:50 +0000

    added macros rewritten print_xmm_float added print_xmm_hex, print_xmm_str
    git-svn-id: file:///mnt/data/SVNrepo@1524 a13746d7-5020-4d46-8679-245a07f888a7

--- sse-aux.c
+++ sse-aux.c
@@ -2,17 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-void print_vect(float* v) {
-	int i;
-	printf("{", i);
-	for (i=0; i < 4; i++) {
-		printf("%12.2f", v[i]);
-		if (i < 3) printf(", ");
-		else printf("}\n");
-	}
-}
+#define packed_byte(x)   {x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x}
+#define packed_word(x)   {x, x, x, x, x, x, x, x}
+#define packed_dword(x)  {x, x, x, x}
+#define packed_float(f)  packed_dword(f)
+#define packed_double(f) {f, f}
 
-void print_xmmps(int i) {
+#define SIMD_ALIGN __attribute__((aligned(16)))
+
+
+#define DUMP_XMM(x)		\
+	"pushal\n"		\
+	"pushl $" ##x "\n"	\
+	"call dump_xmm\n"	\
+	"popl %%eax\n"		\
+	"popal\n"		\
+
+
+void print_xmm_float(int i) {
 	static float v[4];
 	switch (i) {
 		case 0: asm("movups %%xmm0, (%0)":: "r" (v)); break;
@@ -24,16 +31,76 @@ void print_xmmps(int i) {
 		case 6: asm("movups %%xmm6, (%0)":: "r" (v)); break;
 		case 7: asm("movups %%xmm7, (%0)":: "r" (v)); break;
 	}
-	printf("xmm%d = ", i);
-	print_vect(v);
+	printf("xmm%d = {%12.4f, %12.4f, %12.4f, %12.4f}\n", i, v[0], v[1], v[2], v[3]);
 }
 
-float initfrand() {
-	srand(time(NULL));
+
+#define PRINT_XMM_FLOAT(n)
+	"pushal\n"
+	"pushl $" ##n "\n"
+	"call print_xmm_float\n"
+	"popl %%eax\n"
+	"popal\n"
+
+
+void print_xmm_hex(int i) {
+	int k;
+	static uint8_t v[16];
+	switch (i) {
+		case 0: asm("movups %%xmm0, (%0)":: "r" (v)); break;
+		case 1: asm("movups %%xmm1, (%0)":: "r" (v)); break;
+		case 2: asm("movups %%xmm2, (%0)":: "r" (v)); break;
+		case 3: asm("movups %%xmm3, (%0)":: "r" (v)); break;
+		case 4: asm("movups %%xmm4, (%0)":: "r" (v)); break;
+		case 5: asm("movups %%xmm5, (%0)":: "r" (v)); break;
+		case 6: asm("movups %%xmm6, (%0)":: "r" (v)); break;
+		case 7: asm("movups %%xmm7, (%0)":: "r" (v)); break;
+	}
+	printf("xmm%d = {", i);
+	for (k=0; k < 16; k++)
+		printf("%02x", v[i]);
+	printf("}\n");
 }
 
-float frand(float min, float max) {
-	return (max - min)*rand()/(float)RAND_MAX + min;
+
+#define PRINT_XMM_HEX(n)
+	"pushal\n"
+	"pushl $" ##n "\n"
+	"call print_xmm_hex\n"
+	"popl %%eax\n"
+	"popal\n"
+
+
+void print_xmm_str(int i) {
+	int k;
+	static uint8_t v[16];
+	switch (i) {
+		case 0: asm("movups %%xmm0, (%0)":: "r" (v)); break;
+		case 1: asm("movups %%xmm1, (%0)":: "r" (v)); break;
+		case 2: asm("movups %%xmm2, (%0)":: "r" (v)); break;
+		case 3: asm("movups %%xmm3, (%0)":: "r" (v)); break;
+		case 4: asm("movups %%xmm4, (%0)":: "r" (v)); break;
+		case 5: asm("movups %%xmm5, (%0)":: "r" (v)); break;
+		case 6: asm("movups %%xmm6, (%0)":: "r" (v)); break;
+		case 7: asm("movups %%xmm7, (%0)":: "r" (v)); break;
+	}
+	printf("xmm%d = {", i);
+	for (k=0; k < 16; k++)
+		if (v[i] < 0x20)
+			printf("\\x%02x", v[i]);
+		else
+		if (v[i] == '\'')
+			printf("\\'");
+		else
+			putchar(v[i]);
+
+	printf("}\n");
 }
 
-// vim: ts=4 sw=4 nowrap
+
+#define PRINT_XMM_STR(n)
+	"pushal\n"
+	"pushl $" ##n "\n"
+	"call print_xmm_str\n"
+	"popl %%eax\n"
+	"popal\n"
