795d5b51224eae022e38acee66fa17e93bc85c3d
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2008-06-06 13:22:21 +0000

    added function SSE4_blend avoid GCC bug when compiled with -O2/-O3
    git-svn-id: file:///mnt/data/SVNrepo@1586 a13746d7-5020-4d46-8679-245a07f888a7

--- mix_32bpp.c
+++ mix_32bpp.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 #include <sys/time.h>
 #include <time.h>
 
 #include "Xscr.h"
 #include "load_ppm.h"
 
-int width, height;
+int width, height, maxval;
 uint8_t *imgA, *imgB, *data;
 uint8_t alpha;
 
@@ -20,11 +21,14 @@ uint32_t getTime(void) {
 	return (T.tv_sec * 1000000) + T.tv_usec;
 }
 
-
+void SSSE3_blend() __attribute__((noinline));
 void SSSE3_blend() {
 	int n = width * height * 4;
 
 	__asm__ volatile (
+#ifndef GCC_FIXED_BUG
+		"	pushal				\n"
+#endif
 		"	pxor   %%xmm0, %%xmm0		\n"
 		"	movd    %%edx, %%xmm6		\n"
 		"	xorl      $-1, %%edx		\n"
@@ -63,6 +67,9 @@ void SSSE3_blend() {
 		"					\n"
 		"	subl  $1, %%ecx			\n"
 		"	jnz   0b			\n"
+#ifndef GCC_FIXED_BUG
+		"	popal				\n"
+#endif
 		"					\n"
 		: /* no output */
 		: "a" (imgA),
@@ -70,6 +77,69 @@ void SSSE3_blend() {
 		  "D" (data),
 		  "c" (n/16),
 		  "d" (alpha)
+	);
+}
+
+
+void SSE4_blend() __attribute__((noinline));
+void SSE4_blend() {
+	int n = width * height * 4;
+
+	__asm__ volatile (
+#ifndef GCC_FIXED_BUG
+		"	pushal				\n"
+#endif
+		"	pxor   %%xmm0, %%xmm0		\n"
+
+		"	movd    %%edx, %%xmm6		\n"
+		"	xorl	$0x3f, %%edx		\n"
+		"	movd    %%edx, %%xmm7		\n"
+		"	pshufb %%xmm0, %%xmm6		\n"
+		"	pshufb %%xmm0, %%xmm7		\n"
+		"	punpcklbw %%xmm7, %%xmm6	\n"
+		"0:					\n"
+		"	movdqa (%%eax), %%xmm0		\n"
+		"	movdqa (%%ebx), %%xmm2		\n"
+		"	movdqa 16(%%eax), %%xmm3		\n"
+		"	movdqa 16(%%ebx), %%xmm5		\n"
+		"	movdqa  %%xmm0, %%xmm1		\n"
+		"	movdqa  %%xmm3, %%xmm4		\n"
+		"					\n"
+		"	punpcklbw %%xmm2, %%xmm0	\n"
+		"	punpckhbw %%xmm2, %%xmm1	\n"
+		"	punpcklbw %%xmm5, %%xmm3	\n"
+		"	punpckhbw %%xmm5, %%xmm4	\n"
+		"					\n"
+		"	pmaddubsw %%xmm6, %%xmm0	\n"
+		"	pmaddubsw %%xmm6, %%xmm1	\n"
+		"	pmaddubsw %%xmm6, %%xmm3	\n"
+		"	pmaddubsw %%xmm6, %%xmm4	\n"
+		"					\n"
+		"	psrlw        $6, %%xmm0		\n"
+		"	psrlw        $6, %%xmm1		\n"
+		"	psrlw        $6, %%xmm3		\n"
+		"	psrlw        $6, %%xmm4		\n"
+		"	packuswb %%xmm1, %%xmm0		\n"
+		"	packuswb %%xmm4, %%xmm3		\n"
+		"					\n"
+		"	addl $32, %%eax			\n"
+		"	addl $32, %%ebx			\n"
+		"					\n"
+		"	movdqa %%xmm0, (%%edi)		\n"
+		"	movdqa %%xmm3, 16(%%edi)		\n"
+		"	addl $32, %%edi			\n"
+		"					\n"
+		"	subl  $1, %%ecx			\n"
+		"	jnz   0b			\n"
+#ifndef GCC_FIXED_BUG
+		"	popal				\n"
+#endif
+		: /* no output */
+		: "a" (imgA),
+		  "b" (imgB),
+		  "D" (data),
+		  "c" (n/32),
+		  "d" (alpha/4)	// [0..63]
 	);
 }
 
@@ -134,6 +204,10 @@ void motion(
 				printf(" SSSE3");
 				SSSE3_blend();
 				break;
+			case 2:
+				printf(" SSE4");
+				SSE4_blend();
+				break;
 		}
 		t2 = getTime();
 		printf(" = %dus\n", t2-t1);
@@ -166,7 +240,7 @@ void keyboard(
 
 	switch (c) {
 		case XK_space:
-			function = (function + 1) % 2;
+			function = (function + 1) % 3;
 			break;
 
 		case XK_q:
@@ -177,36 +251,199 @@ void keyboard(
 }
 
 
-int main(int argc, char* argv[]) {
-	int result;
-	FILE* f;
-	int maxval;
+#define OPT_COUNT 3
 
-	f = fopen("A.ppm", "rb");
-	ppm_load_32bpp(f, &width, &height, &maxval, &imgA, 1);
+
+static char* function_name[OPT_COUNT] = {
+	"C implementation", "SSSE3", "SSE4"
+};
+
+
+static char* function_name_abbr[OPT_COUNT] = {
+	"x86", "SSSE3", "SSE4"
+};
+
+
+void help(char* progname) {
+	int i;
+
+	printf("1. %s view file1.ppm file2.ppm\n", progname);
+	printf("2. %s measure %s", progname, function_name_abbr[0]);
+
+	for (i=1; i < OPT_COUNT; i++)
+		printf("|%s", function_name_abbr[i]);
+	
+	printf(" repeat-count\n");
+
+	exit(1);
+}
+
+
+void die(const char* fmt, ...) {
+	va_list ap;
+
+	va_start(ap, fmt);
+	vprintf(fmt, ap);
+	putchar('\n');
+	va_end(ap);
+
+	exit(2);
+}
+
+
+void measure(int function, int repeat_count) {
+	uint32_t t1, t2;
+	int n = repeat_count;
+	
+	printf(
+		"function %s, called %d times; image %d x %d\n",
+		function_name[function],
+		repeat_count,
+		width, height
+	);
+
+	switch (function) {
+		case 0:
+			t1 = getTime();
+			while (n--)
+				blend();
+			t2 = getTime();
+			break;
+		
+		case 1:
+			t1 = getTime();
+			while (n--)
+				SSSE3_blend();
+			t2 = getTime();
+			break;
+		
+		case 2:
+			t1 = getTime();
+			while (n--)
+				SSE4_blend();
+			t2 = getTime();
+			break;
+		default:
+			return;
+	}
+
+	printf("time = %d us\n", t2 - t1);
+}
+
+
+void view(const char* file1, const char* file2) {
+	FILE *f;
+	int err;
+
+	f = fopen(file1, "rb");
+	if (f == NULL)
+		die("Can't open %s", file1);
+	
+
+	err = ppm_load_32bpp(f, &width, &height, &maxval, &imgA, 1);
+	if (err < 0)
+		die("Can't read %s: %s", file1, PPM_errormsg[-err]);
+
 	fclose(f);
-	f = fopen("B.ppm", "rb");
+	f = fopen(file2, "rb");
+	if (f == NULL)
+		die("Can't open %s", file2);
 	ppm_load_32bpp(f, &width, &height, &maxval, &imgB, 1);
+	if (err < 0)
+		die("Can't read %s: %s", file2, PPM_errormsg[-err]);
 	fclose(f);
 
-	posix_memalign((void*)&data, 16, width*height*4);
+	printf("%d %d %d\n", data, width, height);
+	if (posix_memalign((void*)&data, 16, width*height*4))
+		die("No free memory");
 
 	memcpy(data, imgA, width*height*4);
 
 	// run mainloop
-	result = Xscr_mainloop(
+	err = Xscr_mainloop(
 		width, height, DEPTH_32bpp, False, data, 
 		keyboard, motion, buttons,
 		"Xscr demo"
 	);
 
 	// check exit status
-	if (result < 0) {
-		printf("Xscr error: %s\n", Xscr_errormsg[-result]);
+	if (err < 0) {
+		printf("Xscr error: %s\n", Xscr_errormsg[-err]);
 	}
 
 	free(imgA);
 	free(imgB);
+	free(data);
+}
+
+
+int main(int argc, char* argv[]) {
+	int result;
+	FILE* f;
+	int maxval;
+
+	int action = -1;
+	int function;
+	int repeat_count;
+
+	// parse args
+	if (argc < 2)
+		help(argv[0]);
+
+	if (strcasecmp(argv[1], "measure") == 0) {
+		action = 1;
+		if (argc < 4)
+			help(argv[0]);
+
+		repeat_count = atoi(argv[3]);
+		if (repeat_count < 0)
+			help(argv[0]);
+
+		width  = 1024;
+		height = 768;
+		alpha  = 127;
+
+		imgA = NULL;
+		imgB = NULL;
+		data = NULL;
+
+		posix_memalign((void*)&imgA, 16, width*height*4);
+		if (imgA == NULL) die("No free memory");
+
+		posix_memalign((void*)&imgB, 16, width*height*4);
+		if (imgB == NULL) die("No free memory");
+
+		posix_memalign((void*)&data, 16, width*height*4);
+		if (data == NULL) die("No free memory");
+
+		for (function=0; function < OPT_COUNT; function++) {
+			if (strcasecmp(argv[2], function_name_abbr[function]) == 0) {
+				measure(function, repeat_count);
+				break;
+			}
+		}
+
+		free(imgA);
+		free(imgB);
+		free(data);
+		
+		if (function == OPT_COUNT)
+			help(argv[0]);
+		else
+			return 0;
+	}
+	else
+	if (strcasecmp(argv[1], "view") == 0) {
+		action = 0;
+		if (argc < 4)
+			help(argv[0]);
+		else
+			view(argv[2], argv[3]);
+	}
+	else
+		help(argv[0]);
+
+	
 	return 0;
 }
 
