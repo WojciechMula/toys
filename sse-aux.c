1242c592e35d6721778db26948609a6d905d058c
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2008-06-17 00:17:36 +0000

    a lot of new functions
    git-svn-id: file:///mnt/data/SVNrepo@1624 a13746d7-5020-4d46-8679-245a07f888a7

--- sse-aux.c
+++ sse-aux.c
@@ -22,13 +22,98 @@ float frand(float min, float max) {
 }
 
 
-void print_vec_float(float v[4]) {
-	printf("{%12.4f, %12.4f, %12.4f, %12.4f}\n", v[0], v[1], v[2], v[3]);
+//=== print 8-bit vectors ================================================
+static void print_vec_int8(int8_t v[16]) {
+	int i = 0;
+	printf("{%3d", v[i]);
+	for (i=1; i < 16; i++)
+		printf(", %3d", v[i]);
+	printf("}\n");
 }
 
 
-void print_xmm_float(int i) {
-	static float v[4];
+static void print_vec_uint8(uint8_t v[16]) {
+	int i = 0;
+	printf("{%3u", v[i]);
+	for (i=1; i < 16; i++)
+		printf(", %3u", v[i]);
+	printf("}\n");
+}
+
+
+static void print_vec_uint8_hex(uint8_t v[16]) {
+	int i = 0;
+	printf("{%02x", v[i]);
+	for (i=1; i < 16; i++)
+		printf(",%02x", v[i]);
+	printf("}\n");
+}
+
+
+//=== print 16-bit vectors ================================================
+static void print_vec_int16(int16_t v[8]) {
+	int i = 0;
+	printf("{%6d", v[i]);
+	for (i=1; i < 8; i++)
+		printf(", %6d", v[i]);
+	printf("}\n");
+}
+
+
+static void print_vec_uint16(uint16_t v[8]) {
+	int i = 0;
+	printf("{%5u", v[i]);
+	for (i=1; i < 8; i++)
+		printf(", %5u", v[i]);
+	printf("}\n");
+}
+
+
+static void print_vec_uint16_hex(uint16_t v[8]) {
+	int i = 0;
+	printf("{%04x", v[i]);
+	for (i=1; i < 8; i++)
+		printf(", %04x", v[i]);
+	printf("}\n");
+}
+
+
+//=== print 32-bit vectors ================================================
+static void print_vec_int32(int32_t v[4]) {
+	int i = 0;
+	printf("{%12d", v[i]);
+	for (i=1; i < 4; i++)
+		printf(", %12d", v[i]);
+	printf("}\n");
+}
+
+
+static void print_vec_uint32(uint32_t v[4]) {
+	int i = 0;
+	printf("{%12u", v[i]);
+	for (i=1; i < 4; i++)
+		printf(", %12u", v[i]);
+	printf("}\n");
+}
+
+
+static void print_vec_uint32_hex(uint32_t v[4]) {
+	int i = 0;
+	printf("{%08x", v[i]);
+	for (i=1; i < 4; i++)
+		printf(", %08x", v[i]);
+	printf("}\n");
+}
+
+
+//=== print float vectors =================================================
+void print_vec_float(float v[4]) {
+	printf("{%12.6f, %12.6f, %12.6f, %12.6f}\n", v[0], v[1], v[2], v[3]);
+}
+
+
+//=== load XMM register ==================================================
+void load_xmm(int i, void* v) {
 	switch (i) {
 		case 0: __asm__ volatile("movups %%xmm0, (%0)":: "r" (v)); break;
 		case 1: __asm__ volatile("movups %%xmm1, (%0)":: "r" (v)); break;
@@ -39,8 +124,89 @@ void print_xmm_float(int i) {
 		case 6: __asm__ volatile("movups %%xmm6, (%0)":: "r" (v)); break;
 		case 7: __asm__ volatile("movups %%xmm7, (%0)":: "r" (v)); break;
 	}
+}
+
+
+//=== print XMM registers ================================================
+void print_xmm_float(int i) {
+	static float v[4];
+	load_xmm(i, (void*)v);
 	printf("xmm%d = {%12.4f, %12.4f, %12.4f, %12.4f}\n", i, v[0], v[1], v[2], v[3]);
 }
+
+
+void print_xmm_uint8(int i) {
+	static uint8_t v[16];
+	load_xmm(i, (void*)v);
+	printf("xmm%d = ", i);
+	print_vec_uint8(v);
+}
+
+
+void print_xmm_int8(int i) {
+	static int8_t v[16];
+	load_xmm(i, (void*)v);
+	printf("xmm%d = ", i);
+	print_vec_int8(v);
+}
+
+
+void print_xmm_uint8_hex(int i) {
+	static uint8_t v[16];
+	load_xmm(i, (void*)v);
+	printf("xmm%d = ", i);
+	print_vec_uint8_hex(v);
+}
+
+
+void print_xmm_uint16(int i) {
+	static uint16_t v[8];
+	load_xmm(i, (void*)v);
+	printf("xmm%d = ", i);
+	print_vec_uint16(v);
+}
+
+
+void print_xmm_int16(int i) {
+	static int16_t v[8];
+	load_xmm(i, (void*)v);
+	printf("xmm%d = ", i);
+	print_vec_int16(v);
+}
+
+
+void print_xmm_uint16_hex(int i) {
+	static uint16_t v[8];
+	load_xmm(i, (void*)v);
+	printf("xmm%d = ", i);
+	print_vec_uint16_hex(v);
+}
+
+
+void print_xmm_uint32(int i) {
+	static uint32_t v[4];
+	load_xmm(i, (void*)v);
+	printf("xmm%d = ", i);
+	print_vec_uint32(v);
+}
+
+
+void print_xmm_int32(int i) {
+	static int32_t v[4];
+	load_xmm(i, (void*)v);
+	printf("xmm%d = ", i);
+	print_vec_int32(v);
+}
+
+
+void print_xmm_uint32_hex(int i) {
+	static uint32_t v[4];
+	load_xmm(i, (void*)v);
+	printf("xmm%d = ", i);
+	print_vec_uint32_hex(v);
+}
+
+
 
 
 #define PRINT_XMM_FLOAT(n)		\
