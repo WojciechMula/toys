b56afc07c1c9656768e836dd3dbb5713ac1f49db
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2009-12-31 16:16:58 +0000

    + test prefix against many files
    git-svn-id: file:///mnt/data/SVNrepo@2013 a13746d7-5020-4d46-8679-245a07f888a7

--- cmpprefix.c
+++ cmpprefix.c
@@ -1,12 +1,17 @@
 /*
 
-	Program checks if file starts with string.
+	Program checks if file(s) starts with string.
 	Nonprintable chars can be passed in format
 	\xhh where h is an hex digit.
 
 	Example:
 
-	cmpprefix file "\xff\xd8\xff\xe0\x00\x10JFIF" && echo "JPEG without EXIF"
+	$ cmpprefix "\xff\xd8\xff\xe0\x00\x10JFIF" file && echo "JPEG without EXIF"
+	JPEG without EXIF
+
+	$ cmpprefix "\xff\xd8\xff\xe0\x00\x10JFIF" file.jpg file.txt cat.jpg
+	file.jpg
+	cat.jpg
 
 	Wojciech Mu³a, 2009-12-25
 	public domain
@@ -24,10 +29,10 @@ void help();
 
 int main(int argc, char* argv[]) {
 
-	FILE* input;
-	char* string;
+	char* prefix;
 	char* buf;
 	int  len;
+	int  i;
 	int  result;
 
 	if (argc < 3) {
@@ -35,66 +40,73 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 	
-	len = strlen(argv[2]);
+	len = strlen(argv[1]);
 	if (len == 0) {
 		fprintf(stderr, "empty string\n");
 		return EXIT_FAILURE;
 	}
 	
-	string = malloc(len+1);
-	if (string == NULL) {
+	prefix = malloc(len+1);
+	if (prefix == NULL) {
 		fprintf(stderr, "can't allocate %d byte(s)\n", len+1);
 		return EXIT_FAILURE;
 	}
 	
 	/* any escapes? */
-	len = parse_string(argv[2], string);
+	len = parse_string(argv[1], prefix);
 #ifdef DEBUG
 	printf("len = %d, ", len);
-	safe_print(string, len);
+	safe_print(prefix, len);
 	putchar('\n');
 #endif
 
 	buf = malloc(len);
 	if (buf == NULL) {
-		free(string);
+		free(prefix);
 		fprintf(stderr, "can't allocate %d byte(s)\n", len);
 		return EXIT_FAILURE;
 	}
 
 	errno = 0;
-	input = fopen(argv[1], "rb");
+	if (argc == 3) {	// just one file
+		result = match(argv[2], prefix, len, buf) ?
+				EXIT_SUCCESS : EXIT_FAILURE;
+	}
+	else // more files
+	for (i=2; i < argc; i++) {
+		result = EXIT_SUCCESS;
+		if (match(argv[i], prefix, len, buf))
+			puts(argv[i]);
+	}
+
+	free(buf);
+	free(prefix);
+
+	return result;
+}
+/*------------------------------------------------------------------------*/
+
+int match(const char* filename, const char* prefix, int preflen, char* buf) {
+	FILE* input;
+	
+	input = fopen(filename, "rb");
 	if (errno) {
 		fprintf(stderr, "Can't open file '%s' (err=%d): %s\n",
-			argv[1],
+			filename,
 			errno,
 			strerror(errno)
 		);
-		free(string);
-		free(buf);
-		return EXIT_FAILURE;
+		return 0;
 	}
 	
-	result = EXIT_SUCCESS;
-	if (fread(buf, 1, len, input) != len) {
-		/* file shorter then string */
-		result = EXIT_FAILURE;
-		goto cleanup;
+	if (fread(buf, 1, preflen, input) != preflen) {
+		/* file shorter then prefix */
+		fclose(input);
+		return 0;
 	}
-
-	if (memcmp(string, buf, len) == 0) {
-		result = EXIT_SUCCESS;
-		goto cleanup;
-	}
-	else {
-		result = EXIT_FAILURE;
-		goto cleanup;
-	}
-cleanup:
-	free(string);
-	free(buf);
+	
 	fclose(input);
-	return result;
+	return (memcmp(prefix, buf, preflen) == 0);
 }
 /*------------------------------------------------------------------------*/
 
