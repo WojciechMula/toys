de9b41c224afae237c74ad1c4b201c2ac2341cdf
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2010-04-26 20:46:42 +0000

    test scripts & results from 2 computers
    git-svn-id: file:///mnt/data/SVNrepo@2123 a13746d7-5020-4d46-8679-245a07f888a7

---
+++ swap_tab_pentiumm.sh
@@ -0,0 +1,25 @@
+# aligned
+./revtab m 0    8	 20000000 all
+./revtab m 0   16	 10000000 all
+./revtab m 0   32	 10000000 all
+./revtab m 0   64	 10000000 all
+./revtab m 0  128	 10000000 all
+./revtab m 0  256	  2000000 all
+./revtab m 0  512	  2000000 all
+./revtab m 0 1024	  1000000 all
+./revtab m 0 2048	   200000 all
+./revtab m 0 4096	   100000 all
+./revtab m 0 8192	   100000 all
+
+# unaligned
+./revtab m 3    9	 20000000 all
+./revtab m 3   19	 10000000 all
+./revtab m 3   38	 10000000 all
+./revtab m 3   66	 10000000 all
+./revtab m 3  127	 10000000 all
+./revtab m 3  253	  2000000 all
+./revtab m 3  517	  2000000 all
+./revtab m 3 1020	  1000000 all
+./revtab m 3 2050	   200000 all
+./revtab m 3 4102	   100000 all
+./revtab m 3 8189	   100000 all
