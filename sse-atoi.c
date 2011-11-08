853f251aff6cccad8b44370f1224080a647666a4
wojtek <wojtek@a13746d7-5020-4d46-8679-245a07f888a7>
2011-11-08 20:21:05 +0000

    + simple_atoi + command line parsing
    git-svn-id: file:///mnt/data/SVNrepo@2368 a13746d7-5020-4d46-8679-245a07f888a7

--- sse-atoi.c
+++ sse-atoi.c
@@ -1,9 +1,28 @@
 /*
+	short info
 	SSE: atoi implementation
+
+	Author: Wojciech Muła
+	e-mail: wojciech_mula@poczta.onet.pl
+	www:    http://0x80.pl/
+
+	License: BSD
+
+	initial release: 2011-11-07
+	$Id$
+
+	----------------------------------------------------------------------
+
+	Compilation:
+
+		gcc -O3
+
+	Usage:
+
+		run program without argument to read help
 
 */
 
-#include <stdio.h>
 #include <stdint.h>
 
 #define SIMD_ALIGN __attribute__((aligned(16)))
@@ -12,7 +31,7 @@
 	{a1, a0, a3, a2, a5, a4, a7, a6, a9, a8, a11, a10, a13, a12, a15, a14} 
 	// swapped adjacent elements!
 
-static uint8_t transform[18][16] SIMD_ALIGN = {
+uint8_t transform[18][16] SIMD_ALIGN = {
 
 	T(0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80),
 	T(   0, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80),
@@ -36,17 +55,17 @@ static uint8_t transform[18][16] SIMD_ALIGN = {
 
 #undef T
 
-#include "programming/snippets/asm/sse-aux.c"
+#include "../snippets/asm/sse-aux.c"
 
-static uint8_t ASCII_lower[16] = packed_byte('0');
-static uint8_t ASCII_upper[16] = packed_byte('9' + 1);
+uint8_t ASCII_lower[16] = packed_byte('0');
+uint8_t ASCII_upper[16] = packed_byte('9' + 1);
 
-static uint16_t mul_add_10[8] SIMD_ALIGN = packed_word(256*10 + 1);
-static uint16_t mul_add_100[8] SIMD_ALIGN = {1, 100, 1, 100, 1, 100, 1, 100};
-static uint16_t mul_add_10000[8] SIMD_ALIGN = {1, 10000, 1, 10000, 1, 10000, 1, 10000};
-static uint32_t mul_1000000000[4] SIMD_ALIGN = {100000000, 0, 100000000, 0}; 
+uint16_t mul_add_10[8] SIMD_ALIGN = packed_word(256*10 + 1);
+uint16_t mul_add_100[8] SIMD_ALIGN = {1, 100, 1, 100, 1, 100, 1, 100};
+uint16_t mul_add_10000[8] SIMD_ALIGN = {1, 10000, 1, 10000, 1, 10000, 1, 10000};
+uint32_t mul_1000000000[4] SIMD_ALIGN = {100000000, 0, 100000000, 0}; 
 
-static uint32_t mask_bits[17] = {
+uint32_t mask_bits[17] = {
 #define mask(n) ((1 << (n)) - 1)
 	mask(0),
 	mask(1),
@@ -85,10 +104,10 @@ int sse_atoi(char* s) {
 		"leal 0x7fffffff(%%ecx), %%ecx		\n"
 		"sarl $31, %%ecx					\n"
 
-		// load digits
+		// load digits (and garbages)
 		"movdqu	(%%eax, %%ebx), %%xmm0		\n"
 		
-		// locate '0'
+		// locate '\0'
 		"pxor %%xmm1, %%xmm1				\n"
 		"pcmpeqb %%xmm0, %%xmm1				\n"
 		"pmovmskb %%xmm1, %%edx				\n"
@@ -101,12 +120,12 @@ int sse_atoi(char* s) {
 		"movdqa	ASCII_upper, %%xmm5			\n"
 		"pcmpgtb %%xmm0, %%xmm4				\n"
 		"pcmpgtb %%xmm0, %%xmm5				\n"
-		"pandn %%xmm5, %%xmm4				\n"
+		"pandn %%xmm5, %%xmm4				\n"	// xmm4[i] := (xmm0[i] >= '0' and xmm[i] <= '9')
 		"pmovmskb %%xmm4, %%ebx				\n"
 		"movl mask_bits(, %%edx, 4), %%eax	\n"
-		"andl %%eax, %%ebx					\n"
+		"andl %%eax, %%ebx					\n"	// mask bytes after '\0'
 		"cmpl %%eax, %%ebx					\n"
-		"movl $0xdeedbeef, %%eax			\n"
+		"xor %%eax, %%eax					\n"	// mark error
 		"jne L1								\n"
 
 		// convert ASCII to numbers
@@ -118,14 +137,14 @@ int sse_atoi(char* s) {
 		"movdqa transform(%%edx), %%xmm2	\n"
 		"pshufb %%xmm2, %%xmm0				\n"
 
-		// join adjacent bytes = form 2-digit numbers
+		// join adjacent digits [bytes] => 2-digit numbers
 		"pmullw mul_add_10, %%xmm0			\n"
 		"psrlw  $8, %%xmm0					\n"
 
-		// join adjacent words = form 4-digit numbers
+		// join adjacent words => form 4-digit numbers
 		"pmaddwd mul_add_100, %%xmm0		\n"
 
-		// join adjacent words again = form 8-digits numbers
+		// join adjacent words again => form 8-digits numbers
 		"pxor %%xmm1, %%xmm1				\n"
 		"packusdw %%xmm1, %%xmm0			\n"
 		"pmaddwd mul_add_10000, %%xmm0		\n"
@@ -136,14 +155,14 @@ int sse_atoi(char* s) {
 		"pmuludq mul_1000000000, %%xmm2		\n"
 		"paddq  %%xmm1, %%xmm2				\n"
 
-		// save 32-bit result 
+		// save 32-bit result
 		"movd %%xmm2, %%eax					\n"
 
-		// negate, if minus char was found
+		// negate, if minus char was found earlier
 		"xorl %%ecx, %%eax					\n"
 		"subl %%ecx, %%eax					\n"
 
-		"L1:"
+		"L1:\n"
 		: "=a" (result)
 		: "a" (s)
 		: "edx", "ebx", "ecx"
@@ -153,12 +172,159 @@ int sse_atoi(char* s) {
 }
 
 
-int main(int argc, char* argv[]) {
-	char* s = argv[1];
+int simple_atoi(char* c) {
+	const char sign = *c;
+	int result = 0;
+	if (sign == '+' || sign == '-')
+		c++;
 
-	if (argc > 1) {
-		int x = sse_atoi(s);
-		printf("'%s' => %d (%x)\n", s, x, x);
+	while (*c) {
+		//const uint8_t d = (uint8_t)*c++;
+		const int d = *c++;
+		if (d >= '0' && d <= '9')
+			result = result * 10 + (d - '0');
+		else
+			return 0;
 	}
-	return 0;
+
+	if (sign == '-')
+		return -result;
+	else
+		return result;
 }
+
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include <stdint.h>
+
+uint32_t get_time(void) {
+	static struct timeval T;
+	gettimeofday(&T, NULL);
+	return (T.tv_sec * 1000000) + T.tv_usec;
+}
+
+void usage() {
+	puts("program verify|atoi|sse numbers [iterations]");
+	exit(0);
+}
+
+
+int main(int argc, char* argv[]) {
+	char** array;
+	char tmp[32];
+	int n;
+	int k;
+	int i, j;
+
+	enum {
+		verify,
+		measure_atoi,
+		measure_sse
+	};
+
+	int function;
+
+	if (argc < 3)
+		usage();
+	
+
+#define keyword(name) (strcasecmp(name, argv[1]) == 0)
+	if (keyword("verify"))
+		function = verify;
+	else if (keyword("atoi"))
+		function = measure_atoi;
+	else if (keyword("sse"))
+		function = measure_sse;
+	else
+		usage();
+#undef keyword
+
+	n = atoi(argv[2]);
+	if (n == 0) {
+		puts("number must be greater then 0");
+		return EXIT_FAILURE;
+	}
+
+	if (argc > 3)
+		k = atoi(argv[3]);
+	else
+		k = 1;
+
+	if (n == 0) {
+		puts("count must be greater then 0");
+		return EXIT_FAILURE;
+	}
+
+	array = malloc(n * sizeof(char*));
+	if (array == NULL) {
+		perror("malloc");
+		return EXIT_FAILURE;
+	}
+
+	// prepare data
+	for (i=0; i < n; i++) {
+		int x = rand();
+		if (rand() % 2)
+			x = -x;
+
+		sprintf(tmp, "%d", x);
+		array[i] = strdup(tmp);
+	}
+
+	// do the job
+	if (function == verify) {
+		puts("verify...");
+		for (i=0; i < n; i++) {
+			int a = atoi(array[i]);
+			int s = sse_atoi(array[i]);
+			if (a != s) {
+				printf("SSE failed at '%s' => %d, should be %d\n",
+						array[i], a, s);
+				return EXIT_FAILURE;
+			}
+		}
+		puts("ok");
+	}
+	else if (function == measure_atoi) {
+		printf("atoi, %d numbers, %d iterations\n", n, k);
+		uint32_t t1, t2;
+		t1  = get_time();
+		for (j=0; j < k; j++) {
+			for (i=0; i < n; i++) {
+				simple_atoi(array[i]);
+				simple_atoi(array[i]);
+				simple_atoi(array[i]);
+				simple_atoi(array[i]);
+			}
+		}
+		t2  = get_time();
+
+		printf("t = %0.3fs\n", (t2 - t1)/1000000.0);
+	}
+	else if (function == measure_sse) {
+		printf("SSE, %d numbers, %d iterations\n", n, k);
+		uint32_t t1, t2;
+		t1  = get_time();
+		for (j=0; j < k; j++) {
+			for (i=0; i < n; i++) {
+				sse_atoi(array[i]);
+				sse_atoi(array[i]);
+				sse_atoi(array[i]);
+				sse_atoi(array[i]);
+			}
+		}
+		t2  = get_time();
+
+		printf("t = %0.3fs\n", (t2 - t1)/1000000.0);
+	}
+	
+	free(array);
+	return EXIT_SUCCESS;
+}
+
+/*
+vim: nowrap ts=4 sw=4
+*/
