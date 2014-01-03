10185f169d54136293f69204b80240fff7d8200e
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2014-01-03 01:29:39 +0100

    mxcsr: triggering divide by zero

---
+++ mxcsr/test_divide_by_zero.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+#include "mxcsr.c"
+
+void divps_by_zero();
+
+int main() {
+	divps_by_zero();
+
+	dump_mxcsr();
+}
+
+#define packed_float(x) {(x), (x), (x), (x)}
+#define packed_value(name, x) float name[4] = packed_float(x)
+
+float zero[4] = packed_float(0);
+float one[4]  = packed_float(1);
+
+void divps_by_zero() {
+
+	__asm__ __volatile__(
+		"movups zero,   %%xmm0\n"
+		"movups	one,    %%xmm1\n"
+		"divps  %%xmm0, %%xmm1\n"
+		:
+		:
+		:
+	);
+}
+
