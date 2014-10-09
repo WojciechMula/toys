159952a25bc8dc5f3cf4dce547cdc735f4c43116
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2014-10-09 21:06:03 +0200

    conv_from_hex: minor

--- conv_from_hex/parse_hex_string.c
+++ conv_from_hex/parse_hex_string.c
@@ -15,13 +15,13 @@ uint32_t parse(char* str) {
 
     assert(strlen(str) >= 4);
 
-    const uint32_t input = bswap(*reinterpret_cast<uint32_t*>(str));
+    const uint32_t input  = bswap(*reinterpret_cast<uint32_t*>(str));
 
-    const uint32_t tmp1  = input & packed32(0x40);
-    const uint32_t nine  = tmp1 >> 3 | tmp1 >> 6; // 9 if char ~ [a-fA-F], 0 otherwise
+    const uint32_t letter = input & packed32(0x40);
+    const uint32_t shift  = letter >> 3 | letter >> 6; // 9 if char ~ [a-fA-F], 0 otherwise
 
-    const uint32_t res   = input + nine;
+    const uint32_t adjusted   = input + shift;
 
-
-    return pext(res, 0x0f0f0f0f);
+    // gather lower nibbles of each byte
+    return pext(adjusted, 0x0f0f0f0f);
 }
