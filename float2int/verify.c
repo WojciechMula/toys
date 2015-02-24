19e4acf3a9eb4189ff6a72ec2b4cad8f80071515
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2015-02-24 23:35:57 +0100

    float2int: better error reporting

--- float2int/verify.c
+++ float2int/verify.c
@@ -4,36 +4,43 @@
 
 
 int main(int argc, char* argv[]) {
-	IEEE745_float number;
+    IEEE745_float number;
 
-	uint64_t i;
-	for (i=0; i < UINT32_MAX; i++) {
-		number.dword = i & 0xffffffff;
+    uint64_t i;
+    for (i=0; i < UINT32_MAX; i++) {
+        number.dword = i & 0xffffffff;
 
-		if ((number.dword & 0xfff) == 0)
-			printf("dword: %08x\n", number.dword);
+        if ((number.dword & 0xfff) == 0)
+            printf("dword: %08x\n", number.dword);
 
-		if (!isnormal(number.value))
-			continue;
+        if (!isnormal(number.value))
+            continue;
 
-		const int   casted    = (int)number.value;
-		const int   converted = float2int(number.value);
-		const int   overflow  = (errno == ERANGE);
+        const int   casted    = (int)number.value;
+        const int   converted = float2int(number.value);
+        const int   overflow  = (errno == ERANGE);
 
-		if (casted != INT32_MIN) {
-			if (casted != converted) {
-				puts("ERROR: CONVERT");
-				printf("\tvalue     : %0.9f\n", number.value);
-				printf("\tcasted    : %d\n", casted);
-				printf("\tfloat2int : %d (overflow=%s)\n", converted, overflow ? "yes" : "no");
-			}
-		} else {
-			if (!overflow) {
-				puts("ERROR: OVERFLOW DETECT");
-				printf("\tvalue     : %0.9f\n", number.value);
-				printf("\tcasted    : %d\n", casted);
-				printf("\tfloat2int : %d (overflow=%s)\n", converted, overflow ? "yes" : "no");
-			}
-		}
-	}
+        if (casted != INT32_MIN) {
+            if (casted != converted) {
+                puts("ERROR: CONVERT");
+                printf("\tvalue     : %0.9f\n", number.value);
+                printf("\tcasted    : %d\n", casted);
+                printf("\tfloat2int : %d (overflow=%s)\n", converted, overflow ? "yes" : "no");
+
+                return EXIT_FAILURE;
+            }
+        } else {
+            if (!overflow) {
+                puts("ERROR: OVERFLOW DETECT");
+                printf("\tvalue     : %0.9f\n", number.value);
+                printf("\tcasted    : %d\n", casted);
+                printf("\tfloat2int : %d (overflow=%s)\n", converted, overflow ? "yes" : "no");
+
+                return EXIT_FAILURE;
+            }
+        }
+    }
+
+    puts("ok");
+    return EXIT_SUCCESS;
 }
