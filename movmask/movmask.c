b68de8fad45939ef1770724abad2e479ab5f4ed7
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2015-02-24 12:33:14 +0100

    movmask: explicit assertions about input word

--- movmask/movmask.c
+++ movmask/movmask.c
@@ -1,9 +1,18 @@
 #include <stdint.h>
 
-uint32_t movmask(const uint32_t input) {
+uint32_t movmask_unsafe(const uint32_t input) {
+
+    assert((input & 0x7f7f7f7f) == 0);
+
 	const uint32_t mult = 0x02040810;
 
 	const uint64_t result = (uint64_t)input * mult;
 
 	return (result >> 32) & 0xff;
+}
+
+
+uint32_t movmask(const uint32_t input) {
+
+    return movmask_unsafe(input & 0x80808080);
 }
