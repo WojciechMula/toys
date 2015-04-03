579a3c979fbc7fe98b91269e9a75c70d3cf57ef2
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2015-04-03 16:50:32 +0200

    conv_from_dec: SSE code with intrinsict functions

--- conv_from_dec/parse.sse.cpp
+++ conv_from_dec/parse.sse.cpp
@@ -28,6 +28,8 @@ uint32_t result[4] SIMD_ALIGN;
 // ------------------------------------------------------------------------
 
 
+// #define INLINE_ASM # 32-bit code
+#if defined(INLINE_ASM)
 uint64_t parse_sse(char* s) {
     //assert(strlen(s) >= 16);
 
@@ -66,5 +68,44 @@ uint64_t parse_sse(char* s) {
 
     return uint64_t(result[0]) * 100000000 + result[2];
 }
+#else
 
+#include <immintrin.h>
+
+
+uint64_t parse_sse(char* s) {
+    //assert(strlen(s) >= 16);
+
+    const __m128i ascii0 = _mm_set1_epi8('0');
+    const __m128i mul_1_100   = _mm_setr_epi16(  100,  1,  100,  1,
+                                                 100,  1,  100,  1);
+    const __m128i mul_10_1000 = _mm_setr_epi16( 1000, 10, 1000, 10,
+                                                1000, 10, 1000, 10);
+    const __m128i mul_1_10000 = _mm_setr_epi16(10000,  1,    0,  0,
+                                               10000,  1,    0,  0);
+
+    // =--------------
+
+    const __m128i input   = _mm_sub_epi8(_mm_loadu_si128((__m128i*)s), ascii0);
+
+    const __m128i t2_even = _mm_srli_epi16(_mm_slli_epi16(input, 8), 8);
+    const __m128i t3_even = _mm_madd_epi16(t2_even, mul_10_1000);
+
+    const __m128i t2_odd  = _mm_srli_epi16(input, 8);
+    const __m128i t4_odd  = _mm_madd_epi16(t2_odd, mul_1_100);
+
+    const __m128i t5      = _mm_add_epi32(t3_even, t4_odd);
+    const __m128i t6      = _mm_or_si128(t5, _mm_srli_epi64(t5, 16));
+    const __m128i t7      = _mm_madd_epi16(t6, mul_1_10000);
+
+    union packed_dword {
+        __m128i  vector;
+        uint32_t element[4];
+    } p;
+
+    p.vector = t7;
+
+    return uint64_t(p.element[0]) * uint64_t(100000000) + p.element[2];
+}
+#endif //defined(INLINE_ASM)
 
