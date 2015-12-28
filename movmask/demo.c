45b66f68738d1541e9b218b8d7c103e4171746d1
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2015-12-28 17:34:25 +0100

    movemask: 64-bit code

--- movmask/demo.c
+++ movmask/demo.c
@@ -18,7 +18,8 @@ uint32_t generate(int x) {
 	return result;
 }
 
-int main() {
+
+int test32() {
 	for (unsigned mask=0; mask < 16; mask++) {
 
 		const uint32_t input  = generate(mask);
@@ -26,8 +27,56 @@ int main() {
 
 		const uint32_t result = movmask(input);
 
-		printf("%08x => %x (expected %x)\n", input, result, mask);
-
-		assert(result == mask);
+        if (result != mask) {
+		    printf("%08x => %x (expected %x)\n", input, result, mask);
+            return 0;
+        }
 	}
+
+    return 1;
+}
+
+
+uint64_t generate64(int x) {
+	assert(x >= 0);
+	assert(x < 256);
+
+	uint64_t result = 0;
+
+	for (int i=0; i < 8; i++) {
+		if (x & (1 << i))
+			result |= 1lu << (8*i + 7);
+	}
+
+	return result;
+}
+
+
+int test64() {
+	for (unsigned mask=0; mask < 256; mask++) {
+
+		const uint64_t input  = generate64(mask);
+		assert((input & 0x7f7f7f7f7f7f7f7flu) == 0);
+
+		const uint64_t result = movmask64(input);
+
+        if (result != mask) {
+            printf("%016lx => %lx (expected %x)\n", input, result, mask);
+            return 0;
+        }
+	}
+
+    return 1;
+}
+
+
+int main() {
+    int result = 0;
+    puts("testing 32-bit code...");
+    result = test32() || result;
+
+    puts("testing 64-bit code...");
+    result = test64() || result;
+ 
+    puts(result ? "OK" : "FAILED");
 }
--- movmask/movmask.c
+++ movmask/movmask.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdint.h>
 
 uint32_t movmask_unsafe(const uint32_t input) {
 
@@ -8,11 +9,37 @@ uint32_t movmask_unsafe(const uint32_t input) {
 
 	const uint64_t result = (uint64_t)input * mult;
 
-	return (result >> 32) & 0xff;
+	return (result >> 32) & 0x0f;
 }
 
 
 uint32_t movmask(const uint32_t input) {
 
     return movmask_unsafe(input & 0x80808080);
+}
+
+
+uint64_t movmask64_unsafe(const uint64_t input) {
+
+    assert((input & 0x7f7f7f7f7f7f7f7flu) == 0);
+
+    const uint64_t mult = (1lu << (1*8 - 0))
+                        | (1lu << (2*8 - 1))
+                        | (1lu << (3*8 - 2))
+                        | (1lu << (4*8 - 3))
+                        | (1lu << (5*8 - 4))
+                        | (1lu << (6*8 - 5))
+                        | (1lu << (7*8 - 6))
+                        | (1lu << (8*8 - 7))
+                        ;
+
+    const unsigned __int128 result = (unsigned __int128)mult * (unsigned __int128)input;
+
+    return (result >> 64) & 0xff;
+}
+
+
+uint64_t movmask64(const uint64_t input) {
+
+    return movmask64_unsafe(input & 0x8080808080808080lu);
 }
