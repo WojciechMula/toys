20011b3747023c56e8689f7c44627a0daed9489e
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-03-12 18:34:24 +0100

    base64: helper function (for debugging)

---
+++ base64/debug_dump.cpp
@@ -0,0 +1,21 @@
+void dump_hex(const uint8_t* buf, size_t num) {
+    for (size_t i=0; i < num; i++) {
+        printf("%02x", buf[i]);
+    }
+}
+
+
+void dump(__m128i xmm) {
+    
+    uint8_t buf[16];
+    _mm_storeu_si128(reinterpret_cast<__m128i*>(buf), xmm);
+    dump_hex(buf, 16);
+}
+
+
+void dump(const char* name, __m128i xmm) {
+    
+    printf("%s = [", name);
+    dump(xmm);
+    printf("]\n");
+}
