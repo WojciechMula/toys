50afad71359310340693549de3b4a7a9091d449a
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-05-01 19:27:50 +0200

    mix_32bpp: incr resolution thanks to Dmitry Petrov
    Pmaddusb method: Dmitry noticed that alpha could be a 7-bit value,
    overflow will never occur.

--- mix_32bpp/mix_32bpp.c
+++ mix_32bpp/mix_32bpp.c
@@ -135,8 +135,8 @@ void SSE42_blend() {
 	int n = width * height * 4;
 	int dummy __attribute__((unused));
 
-    const uint8_t alpha2 = alpha/4;
-    __m128i alpha_np = _mm_set1_epi16(alpha2 | ((uint16_t)(alpha2 ^ 0x3f) << 8));
+    const uint8_t alpha2 = alpha/2;
+    __m128i alpha_np = _mm_set1_epi16(alpha2 | ((uint16_t)(alpha2 ^ 0x7f) << 8));
 
     for (size_t i=0; i < n; i += 32) {
         __m128i A0 = _mm_load_si128((__m128i*)(imgA + i));
@@ -156,10 +156,10 @@ void SSE42_blend() {
         hi0 = _mm_maddubs_epi16(hi0, alpha_np);
         hi1 = _mm_maddubs_epi16(hi1, alpha_np);
 
-        lo0 = _mm_srli_epi16(lo0, 6);
-        lo1 = _mm_srli_epi16(lo1, 6);
-        hi0 = _mm_srli_epi16(hi0, 6);
-        hi1 = _mm_srli_epi16(hi1, 6);
+        lo0 = _mm_srli_epi16(lo0, 7);
+        lo1 = _mm_srli_epi16(lo1, 7);
+        hi0 = _mm_srli_epi16(hi0, 7);
+        hi1 = _mm_srli_epi16(hi1, 7);
 
         __m128i res0 = _mm_packus_epi16(lo0, hi0);
         __m128i res1 = _mm_packus_epi16(lo1, hi1);
