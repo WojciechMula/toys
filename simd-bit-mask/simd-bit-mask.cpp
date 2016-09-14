22340183e99b9e20030beebe6c311028f1955435
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-09-14 21:15:45 +0200

    simd-bit-mask: cleanup

--- simd-bit-mask/simd-bit-mask.cpp
+++ simd-bit-mask/simd-bit-mask.cpp
@@ -4,12 +4,12 @@
 
 // input:    k - number of bits
 // returns:  a vector where bits from k to max are set.
-__m256i mask(int i) {
+__m256i mask(int k) {
     // assert(i >= 0 && i <= 256);
 
     const size_t chunk_size = 32;
-    const size_t n     = i / chunk_size;
-    const size_t shift = i % chunk_size;
+    const size_t n     = k / chunk_size;
+    const size_t shift = k % chunk_size;
 
     const __m256i chunk_numbers = _mm256_setr_epi32(0, 1, 2, 3, 4, 5, 6, 7);
     const __m256i chunk         = _mm256_set1_epi32(n);
@@ -18,9 +18,10 @@ __m256i mask(int i) {
     const __m256i tmp1 = _mm256_cmpgt_epi32(chunk_numbers, chunk);
 
     // shift bits in n-th chunk left by required amount
-    const __m256i tmp2 = _mm256_slli_epi32(_mm256_cmpeq_epi32(chunk_numbers, chunk), shift);
+    const __m256i tmp2 = _mm256_cmpeq_epi32(chunk_numbers, chunk);
+    const __m256i tmp3 = _mm256_slli_epi32(tmp2, shift);
 
-    return _mm256_or_si256(tmp1, tmp2);
+    return _mm256_or_si256(tmp1, tmp3);
 }
 
 void _mm256_dump_bin(const __m256i& v);
