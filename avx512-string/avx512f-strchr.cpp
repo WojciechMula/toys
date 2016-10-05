7006455c1246fc1e07ef0b2005dc836045d7a8db
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2016-10-05 18:25:29 +0200

    AVX512 string: faster strchr

--- avx512-string/avx512f-strchr.cpp
+++ avx512-string/avx512f-strchr.cpp
@@ -1,4 +1,4 @@
-char* avx512f_strchr(const char* s, char c) {
+char* avx512f_strchr__reference_implementation(const char* s, char c) {
 
     const __m512i v01 = _mm512_set1_epi32(0x01010101u);
     const __m512i v80 = _mm512_set1_epi32(0x80808080u);
@@ -56,3 +56,51 @@ char* avx512f_strchr(const char* s, char c) {
     }
 }
 
+
+char* avx512f_strchr(const char* s, char c) {
+
+    const __m512i v01 = _mm512_set1_epi32(0x01010101u);
+    const __m512i v80 = _mm512_set1_epi32(0x80808080u);
+    const __m512i ccc = _mm512_set1_epi32(0x01010101u * uint32_t(c));
+
+    char* ptr = const_cast<char*>(s);
+
+    while (true) {
+
+        const __m512i v = _mm512_loadu_si512(ptr);
+
+        const __m512i   vc        = _mm512_xor_si512(v,  ccc);
+        const __m512i   v1        = _mm512_sub_epi32(v, v01);
+        const __m512i   vc1       = _mm512_sub_epi32(vc, v01);
+        const __m512i   tmp1      = _mm512_ternarylogic_epi32(v1, v, v80, 0x20);
+        const __m512i   tmp2      = _mm512_ternarylogic_epi32(vc1, vc, v80, 0x20);
+        const __mmask16 zero_mask = _mm512_test_epi32_mask(tmp1, tmp1);
+        const __mmask16 char_mask = _mm512_test_epi32_mask(tmp2, tmp2);
+
+        if (!_mm512_kortestz(zero_mask, char_mask)) {
+            const size_t n = __builtin_ctz(zero_mask | char_mask); // number of dword
+
+            char t;
+
+            t = ptr[4*n + 0];
+            if (t == 0) return NULL;
+            if (t == c) return ptr + 4*n + 0;
+
+            t = ptr[4*n + 1];
+            if (t == 0) return NULL;
+            if (t == c) return ptr + 4*n + 1;
+
+            t = ptr[4*n + 2];
+            if (t == 0) return NULL;
+            if (t == c) return ptr + 4*n + 2;
+
+            t = ptr[4*n + 3];
+            if (t == 0) return NULL;
+            if (t == c) return ptr + 4*n + 3;
+
+            assert(false && "unxpected invocation");
+        }
+
+        ptr += 64;
+    }
+}
