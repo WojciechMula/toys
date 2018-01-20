4c98921cde222e37320dac7df8dd040be6c5a6c2
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-01-20 10:55:08 +0100

    Remove unnecessary includes

--- fbstp/fbstp_speed.c
+++ fbstp/fbstp_speed.c
@@ -1,7 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <time.h>
 #include <sys/time.h>
 
 
--- fbstp/fbstp_test.c
+++ fbstp/fbstp_test.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdint.h>
 
 
