423337f8ef49096f1e96648be5a844649b050a3e
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-03-03 18:40:24 +0100

    Intersection algorithm that use binary search

--- set-intersection/main.cpp
+++ set-intersection/main.cpp
@@ -65,7 +65,7 @@ void sse_set_intersection(const vec& A, const vec& B, INSERTER output) {
         const __m128i lt = _mm_cmplt_epi32(b, a_rep);
         const uint16_t mask = _mm_movemask_epi8(lt);
         if (mask == 0xffff) {
-            // all elements in b are smaller, fetch next chunk from B
+            // all elements in b are smaller, fetch the next chunk from B
             bi += 4;
             b = _mm_loadu_si128(reinterpret_cast<const __m128i*>(&B[bi]));
         } else {
@@ -78,15 +78,36 @@ void sse_set_intersection(const vec& A, const vec& B, INSERTER output) {
                 }
             }
 
-            // fetch next value from A
+            // fetch the next value from A
             ai += 1;
             a_rep = _mm_set1_epi32(A[ai]);
         }
     }
 }
 
+template <typename INSERTER>
+void binsearch_set_intersection(const vec& A, const vec& B, INSERTER output) {
+
+    auto it = B.begin();
+
+    for (const auto& a: A) {
+        it = std::lower_bound(it, B.end(), a);
+        if (it == B.end()) {
+            // since there are no values greater or equal the current,
+            // there also won't be any for subsequent value (all greater than current)
+            return;
+        }
+
+        if (*it == a) {
+            output = a;
+            ++it;
+        }
+    }
+}
+
 constexpr int STD = 0;
 constexpr int SSE = 1;
+constexpr int BINARY = 2;
 
 template <int version = STD>
 void test(const char* info, const vec& a, const vec& b, int k) {
@@ -105,6 +126,8 @@ void test(const char* info, const vec& a, const vec& b, int k) {
             std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(result));
         } else if constexpr (version == SSE) {
             sse_set_intersection(a, b, std::back_inserter(result));
+        } else if constexpr (version == BINARY) {
+            binsearch_set_intersection(a, b, std::back_inserter(result));
         }
         const auto t2 = Clock::now();
         best_time += elapsed(t1, t2);
@@ -123,4 +146,5 @@ int main() {
 
     test<STD>("std", vec1, vec2, 1000);
     test<SSE>("SSE", vec1, vec2, 1000);
+    test<BINARY>("binsearch", vec1, vec2, 1000);
 }
