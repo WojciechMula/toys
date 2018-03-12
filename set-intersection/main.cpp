b56599104dca3f41a1e818c8bba65eeb514cbf86
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-03-12 20:17:52 +0100

    move test code to a class

--- set-intersection/main.cpp
+++ set-intersection/main.cpp
@@ -16,57 +16,73 @@ constexpr int STD = 0;
 constexpr int SSE = 1;
 constexpr int BINARY = 2;
 
-template <int version>
-void test(const char* info, const vec& a, const vec& b, int k) {
+class Application final {
 
-    vec result;
-    volatile int32_t ref = 0;
+    vec input_vec;
+    static constexpr size_t input_size = 1024*1024;
+    static constexpr size_t iterations = 1000;
 
-    printf("%s [a.size = %lu, b.size = %lu, %d iterations] ", info, a.size(), b.size(), k);
-    fflush(stdout);
+public:
+    void run() {
 
-    Clock::rep best_time = 0;
-    int tmp = k;
-    while (tmp-- > 0) {
-        result.clear();
-        const auto t1 = Clock::now();
-        if constexpr (version == STD) {
-            std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(result));
-        } else if constexpr (version == SSE) {
-            sse_set_intersection(a, b, std::back_inserter(result));
-        } else if constexpr (version == BINARY) {
-            binsearch_set_intersection(a, b, std::back_inserter(result));
+        measure_time("create input table: ", [this]{
+            input_vec = create_sorted(input_size);
+        });
+
+        for (size_t base_size: {128, 1024}) {
+            for (size_t factor: {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}) {
+                const size_t size = base_size * factor;
+                test_all(size);
+            }
         }
-        const auto t2 = Clock::now();
-        best_time += elapsed(t1, t2);
 
-        ref += std::accumulate(result.begin(), result.end(), int32_t(0));
     }
 
-    printf("%lu us (%d)\n", best_time, ref);
-}
+private:
+    template <int version>
+    void test(const char* info, const vec& a, const vec& b, int k) {
 
-void test_all(const vec& larger, size_t size, int iterations) {
+        vec result;
+        volatile int32_t ref = 0;
 
-    auto smaller = sample_sorted(larger, size);
+        printf("%s [a.size = %lu, b.size = %lu, %d iterations] ", info, a.size(), b.size(), k);
+        fflush(stdout);
 
-    test<STD>("std",          smaller, larger, iterations);
-    test<SSE>("SSE",          smaller, larger, iterations);
-    test<BINARY>("binsearch", smaller, larger, iterations);
-}
+        Clock::rep best_time = 0;
+        int tmp = k;
+        while (tmp-- > 0) {
+            result.clear();
+            const auto t1 = Clock::now();
+            if constexpr (version == STD) {
+                std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(result));
+            } else if constexpr (version == SSE) {
+                sse_set_intersection(a, b, std::back_inserter(result));
+            } else if constexpr (version == BINARY) {
+                binsearch_set_intersection(a, b, std::back_inserter(result));
+            }
+            const auto t2 = Clock::now();
+            best_time += elapsed(t1, t2);
+
+            ref += std::accumulate(result.begin(), result.end(), int32_t(0));
+        }
+
+        printf("%lu us (%d)\n", best_time, ref);
+    }
+
+    void test_all(size_t size) {
+
+        auto sampled = sample_sorted(input_vec, size);
+
+        test<STD>("std",          sampled, input_vec, iterations);
+        test<SSE>("SSE",          sampled, input_vec, iterations);
+        test<BINARY>("binsearch", sampled, input_vec, iterations);
+    }
+
+};
 
 int main() {
 
-    vec input_vec;
-    measure_time("create input table: ", [&input_vec]{
-        input_vec = create_sorted(1024*1024);
-    });
-
-    for (size_t base_size: {128, 1024}) {
-        for (size_t factor: {1,2,3,4,5,6,7,8,9,10}) {
-            const size_t size = base_size * factor;
-            test_all(input_vec, size, 1000);
-        }
-    }
+    Application app;
+    app.run();
 }
 
