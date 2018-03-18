58f45b25fdc3a3321ee0c6b1aa4a4c1ec17d8ae2
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-03-18 16:30:59 +0100

    Extract procedures set to an extra file

--- sse-utoa/sse-utoa.c
+++ sse-utoa/sse-utoa.c
@@ -68,38 +68,7 @@ char* utoa64(const uint64_t x) {
 }
 
 
-//----------------------------------------------------------------------
-//--- SSE implementations ----------------------------------------------
-//----------------------------------------------------------------------
-
-// magick numbers for 16-bit division
-#define DIV_10		0x199a	// shift = 0 + 16
-#define DIV_100		0x147b	// shift = 3 + 16
-
-// magic number for 32-bit division
-#define DIV_10000	0xd1b71759		// shift = 13 + 32
-
-#define SIMD_ALIGN __attribute__((aligned(16)))
-
-//---- output buffer for SSE procedures --------------------------------
-static char utoa_buffer[32] SIMD_ALIGN;
-
-#ifdef USE_ASM
-
-#include "asm-helpers.c"
-#include "sse32-asm.c"
-#include "sse32_2-asm.c"
-#include "sse64-asm.c"
-
-#else
-
-#include <immintrin.h>
-#include "sse32-intrin.c"
-#include "sse32_2-intrin.c"
-#include "sse64-intrin.c"
-
-#endif
-
+#include "procedures.c"
 
 //----------------------------------------------------------------------
 
