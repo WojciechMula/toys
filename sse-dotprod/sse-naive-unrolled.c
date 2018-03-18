eb8e7aec87c205c3d071ac0c89b3dbeb4815c0e6
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-03-18 22:29:01 +0100

    Use separate accumulator

--- sse-dotprod/sse-naive-unrolled.c
+++ sse-dotprod/sse-naive-unrolled.c
@@ -1,8 +1,11 @@
 float sse_naive_unrolled(const float* a, const float* b, size_t n) {
-    
+
     const size_t chunks = n/(4*4);
 
-    __m128 dots = _mm_setzero_ps();
+    __m128 dots0 = _mm_setzero_ps();
+    __m128 dots1 = _mm_setzero_ps();
+    __m128 dots2 = _mm_setzero_ps();
+    __m128 dots3 = _mm_setzero_ps();
     for (size_t i=0; i < n; i += 4*4) {
         __m128 A0 = _mm_loadu_ps(&a[i + 0*4]);
         __m128 A1 = _mm_loadu_ps(&a[i + 1*4]);
@@ -19,15 +22,17 @@ float sse_naive_unrolled(const float* a, const float* b, size_t n) {
         __m128 AB2 = _mm_mul_ps(A2, B2);
         __m128 AB3 = _mm_mul_ps(A3, B3);
 
-        dots = _mm_add_ps(dots, AB0);
-        dots = _mm_add_ps(dots, AB1);
-        dots = _mm_add_ps(dots, AB2);
-        dots = _mm_add_ps(dots, AB3);
+        dots0 = _mm_add_ps(dots0, AB0);
+        dots1 = _mm_add_ps(dots1, AB1);
+        dots2 = _mm_add_ps(dots2, AB2);
+        dots3 = _mm_add_ps(dots3, AB3);
     }
 
-    float tmp[4];
-    _mm_storeu_ps(tmp, dots);
-    float result = dots[0] + dots[1] + dots[2] + dots[3];
+    dots0 = _mm_add_ps(dots0, dots1);
+    dots2 = _mm_add_ps(dots2, dots3);
+    dots0 = _mm_add_ps(dots0, dots2);
+
+    float result = dots0[0] + dots0[1] + dots0[2] + dots0[3];
 
     for (size_t i=chunks*4*4; i < n; i++) {
         result += a[i] * b[i];
