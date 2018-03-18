51f001b6076fd7e15ac863ce6845e7ee7fa84f73
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-03-18 15:12:13 +0100

    Tune comments

--- sse-utoa/sse-intrin-utoa.c
+++ sse-utoa/sse-intrin-utoa.c
@@ -132,49 +132,72 @@ static char utoa_buffer[32] SIMD_ALIGN;
 
 
 char* utoa32_sse(uint32_t v) {
-    // number of digits in v at most 8
+    // v is a 8-digit number: abcdefh (a..h are decimal digits)
     __m128i x = _mm_cvtsi32_si128(v);
 
-	// x div 10^4
-    __m128i x_div_10000 = _mm_mul_epu32(x, _mm_set1_epi32(DIV_10000));
-    x_div_10000 = _mm_srli_epi64(x_div_10000, 45);
+    const __m128i div_10000 = _mm_set1_epi32(DIV_10000);
+    const __m128i mul_10000 = _mm_set1_epi32(10000);
+    const int div_10000_shift = 45;
 
-    // x mod 10^4
-    __m128i x_mod_10000 = _mm_mul_epu32(x_div_10000, _mm_set1_epi32(10000));
+    const __m128i div_100   = _mm_set1_epi16(DIV_100);
+    const __m128i mul_100   = _mm_set1_epi16(100);
+    const int div_100_shift = 3;
+
+    const __m128i div_10    = _mm_set1_epi16(DIV_10);
+    const __m128i mul_10    = _mm_set1_epi16(10);
+
+    const __m128i ascii0    = _mm_set1_epi8('0');
+    
+    //                [ 3 | 2 | 1 | 0 | 3 | 2 | 1 | 0 | 3 | 2 | 1 | 0 | 3 | 2 | 1 | 0 ]
+    // x            = [       0       |       0       |       0       |      abcdefgh ]
+	// x div 10^4   = [       0       |       0       |       0       |          abcd ]
+    __m128i x_div_10000;
+    x_div_10000 = _mm_mul_epu32(x, div_10000);
+    x_div_10000 = _mm_srli_epi64(x_div_10000, div_10000_shift);
+
+    // x mod 10^4   = [       0       |       0       |       0       |          efgh ]
+    __m128i x_mod_10000;
+    x_mod_10000 = _mm_mul_epu32(x_div_10000, mul_10000);
     x_mod_10000 = _mm_sub_epi32(x, x_mod_10000);
 
-    // y = packed_word([x div 10000 | x mod 10000 ])
-    //   = [ abcd | efgh | .... ] - four-decimal-digit numbers in each 16-bit element (a..h - digits)
+    // y            = [   0   |   0   |   0   |   0   |   0   |   0   |  abcd |  efgh ]
     __m128i y = _mm_unpacklo_epi16(x_div_10000, x_mod_10000);
     
-    // y_mod_100 = packed_word([ 00ab | 00ef | ... ])
-    __m128i y_div_100 = _mm_mulhi_epu16(y, _mm_set1_epi16(DIV_100));
-    y_div_100 = _mm_srli_epi16(y_div_100, 3);
+    // y_div_100    = [   0   |   0   |   0   |   0   |   0   |   0   |    ab |    ef ]
+    __m128i y_div_100;
+    y_div_100 = _mm_mulhi_epu16(y, div_100);
+    y_div_100 = _mm_srli_epi16(y_div_100, div_100_shift);
 
-    // y_div_100 = packed_word([ 00cd | 00gh | ... ])
-    __m128i y_mod_100 = _mm_mullo_epi16(y_div_100, _mm_set1_epi16(100));
+    // y_mod_100    = [   0   |   0   |   0   |   0   |   0   |   0   |    cd |    gh ]
+    __m128i y_mod_100;
+    y_mod_100 = _mm_mullo_epi16(y_div_100, mul_100);
     y_mod_100 = _mm_sub_epi16(y, y_mod_100);
 
-    // z = packed_word([ 00ab | 00cd | 00ef | 00hg | ... ])
+    // z            = [   0   |   0   |   0   |   0   |    ab |    cd |    ef |    gh ]
     __m128i z = _mm_unpacklo_epi16(y_div_100, y_mod_100);
 
-    // z_div_10 = packed_word([ 000a | 000c | 000e | 000h | ... ])
-    __m128i z_div_10 = _mm_mulhi_epu16(z, _mm_set1_epi16(DIV_10));
+    // z_div_10     = [   0   |   0   |   0   |   0   |     a |     c |     e |     g ]
+    __m128i z_div_10 = _mm_mulhi_epu16(z, div_10);
 
-    // z_mod_10 = packed_word([ 000b | 000d | 000f | 000g | ... ])
-    __m128i z_mod_10 = _mm_mullo_epi16(z_div_10, _mm_set1_epi16(10));
+    // z_mod_10     = [   0   |   0   |   0   |   0   |     b |     d |     f |     h ]
+    __m128i z_mod_10;
+    z_mod_10 = _mm_mullo_epi16(z_div_10, mul_10);
     z_mod_10 = _mm_sub_epi16(z, z_mod_10);
 
     // interleave z_mod_10 and z_div_10 - it's still a word vector
-    __m128i tmp = _mm_unpacklo_epi16(z_div_10, z_mod_10);
-    tmp = _mm_packus_epi16(_mm_setzero_si128(), tmp); // now it's a byte vector
+    __m128i tmp;
+    // tmp          = [   a   |   b   |   c   |   d   |   e   |   f   |   g   |   h   ]
+    tmp = _mm_unpacklo_epi16(z_div_10, z_mod_10);
+
+    // tmp          = [ 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | a | b | c | d | e | f | g | h ]
+    tmp = _mm_packus_epi16(_mm_setzero_si128(), tmp);
 
     // determine number of leading zeros
     uint16_t mask = ~_mm_movemask_epi8(_mm_cmpeq_epi8(tmp, _mm_setzero_si128()));
     uint32_t offset = __builtin_ctz(mask | 0x8000);
 
     // convert to ascii
-    tmp = _mm_add_epi8(tmp, _mm_set1_epi8('0'));
+    tmp = _mm_add_epi8(tmp, ascii0);
 
     // and save result
     _mm_storeu_si128((__m128i*)utoa_buffer, tmp);
@@ -272,45 +295,79 @@ void dump_epi16(__m128i x) {
     printf("]\n");
 }
 
+void dump_epi8(__m128i x) {
+    uint8_t dump[16];
+
+    _mm_storeu_si128((__m128i*)dump, x);
+    putchar('[');
+    for (int i=15; i >= 0; i--) {
+        if (i < 15) putchar('|');
+        printf(" %2d ", dump[i]);
+    }
+    printf("]\n");
+}
+
 char* utoa64_sse(uint64_t v) {
 
-	// can't be easliy done in SSE
-	uint32_t a = v / 100000000;
-	uint32_t b = v % 100000000;
+    // v is 16-digit number = abcdefghijklmnop
 
+    const __m128i div_10000 = _mm_set1_epi32(DIV_10000);
+    const __m128i mul_10000 = _mm_set1_epi32(10000);
+    const int div_10000_shift = 45;
+
+    const __m128i div_100   = _mm_set1_epi16(DIV_100);
+    const __m128i mul_100   = _mm_set1_epi16(100);
+    const int div_100_shift = 3;
+
+    const __m128i div_10    = _mm_set1_epi16(DIV_10);
+    const __m128i mul_10    = _mm_set1_epi16(10);
+
+    const __m128i ascii0    = _mm_set1_epi8('0');
+
+	// can't be easliy done in SSE
+	const uint32_t a = v / 100000000; // 8-digit number: abcdefgh
+	const uint32_t b = v % 100000000; // 8-digit number: ijklmnop
+
+    //                [ 3 | 2 | 1 | 0 | 3 | 2 | 1 | 0 | 3 | 2 | 1 | 0 | 3 | 2 | 1 | 0 ]
+    // x            = [       0       |      ijklmnop |       0       |      abcdefgh ]
     __m128i x = _mm_set_epi64x(b, a);
 
-	// x div 10^4
-    __m128i x_div_10000 = _mm_mul_epu32(x, _mm_set1_epi32(DIV_10000));
-    x_div_10000 = _mm_srli_epi64(x_div_10000, 45);
+	// x div 10^4   = [       0       |          ijkl |       0       |          abcd ]
+    __m128i x_div_10000;
+    x_div_10000 = _mm_mul_epu32(x, div_10000);
+    x_div_10000 = _mm_srli_epi64(x_div_10000, div_10000_shift);
 
-    // x mod 10^4
-    __m128i x_mod_10000 = _mm_mul_epu32(x_div_10000, _mm_set1_epi32(10000));
+    // x mod 10^4   = [       0       |          mnop |       0       |          efgh ]
+    __m128i x_mod_10000;
+    x_mod_10000 = _mm_mul_epu32(x_div_10000, mul_10000);
     x_mod_10000 = _mm_sub_epi32(x, x_mod_10000);
 
-    // y = packed_word([a div 10000 | a mod 10000 | b div 10000 | b mod 10000 ])
-    //   = [ abcd | efgh | .... ] - four-decimal-digit numbers in each 16-bit element (a..h - digits)
+    // y            = [          mnop |          ijkl |          efgh |          abcd ]
     __m128i y = _mm_or_si128(x_div_10000, _mm_slli_epi64(x_mod_10000, 32));
     
-    // y_mod_100 = packed_word([ 00ab | 00ef | ... ])
-    __m128i y_div_100 = _mm_mulhi_epu16(y, _mm_set1_epi16(DIV_100));
-    y_div_100 = _mm_srli_epi16(y_div_100, 3);
+    // y_div_100    = [   0   |    mn |   0   |    ij |   0   |    ef |   0   |    ab ]
+    __m128i y_div_100;
+    y_div_100 = _mm_mulhi_epu16(y, div_100);
+    y_div_100 = _mm_srli_epi16(y_div_100, div_100_shift);
 
-    // y_div_100 = packed_word([ 00cd | 00gh | ... ])
-    __m128i y_mod_100 = _mm_mullo_epi16(y_div_100, _mm_set1_epi16(100));
+    // y_mod_100    = [   0   |    op |   0   |    kl |   0   |    gh |   0   |    cd ]
+    __m128i y_mod_100;
+    y_mod_100 = _mm_mullo_epi16(y_div_100, mul_100);
     y_mod_100 = _mm_sub_epi16(y, y_mod_100);
 
-    // z = packed_word([ 00ab | 00cd | 00ef | 00hg | ... ])
+    // z            = [    mn |    op |    ij |    kl |    ef |    gh |    ab |    cd ]
     __m128i z = _mm_or_si128(y_div_100, _mm_slli_epi32(y_mod_100, 16));
 
-    // z_div_10 = packed_word([ 000a | 000c | 000e | 000h | ... ])
-    __m128i z_div_10 = _mm_mulhi_epu16(z, _mm_set1_epi16(DIV_10));
+    // z_div_10     = [ 0 | m | 0 | o | 0 | i | 0 | k | 0 | e | 0 | g | 0 | a | 0 | c ]
+    __m128i z_div_10 = _mm_mulhi_epu16(z, div_10);
 
-    // z_mod_10 = packed_word([ 000b | 000d | 000f | 000g | ... ])
-    __m128i z_mod_10 = _mm_mullo_epi16(z_div_10, _mm_set1_epi16(10));
+    // z_mod_10     = [ 0 | n | 0 | p | 0 | j | 0 | l | 0 | f | 0 | h | 0 | b | 0 | d ]
+    __m128i z_mod_10;
+    z_mod_10 = _mm_mullo_epi16(z_div_10, mul_10);
     z_mod_10 = _mm_sub_epi16(z, z_mod_10);
 
-    // interleave z_mod_10 and z_div_10 - it's still a word vector
+    // interleave z_mod_10 and z_div_10 -
+    // tmp          = [ m | n | o | p | i | j | k | l | e | f | g | h | a | b | c | d ]
     __m128i tmp = _mm_or_si128(z_div_10, _mm_slli_epi16(z_mod_10, 8));
 
     // determine number of leading zeros
@@ -323,7 +380,7 @@ char* utoa64_sse(uint64_t v) {
     // and save result
     _mm_storeu_si128((__m128i*)utoa_buffer, tmp);
 
-	return &utoa_buffer[offset];
+    return &utoa_buffer[offset];
 }
 
 //----------------------------------------------------------------------
