9aca03018ceefc66f980a3cdab1d45c1e32f1455
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-04-04 20:51:47 +0200

    A new helper

---
+++ 000helpers/time_utils.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <chrono>
+
+using Clock = std::chrono::high_resolution_clock;
+
+template <typename UNIT = std::chrono::microseconds>
+Clock::time_point::rep elapsed(const Clock::time_point& t1, const Clock::time_point& t2) {
+    return std::chrono::duration_cast<UNIT>(t2 - t1).count();
+}
+
+template <typename FUN>
+Clock::time_point::rep measure_time(const char* info, FUN fun) {
+
+    printf("%s", info);
+    fflush(stdout);
+    const auto t1 = Clock::now();
+    fun();
+    const auto t2 = Clock::now();
+
+    const auto dt = elapsed(t1, t2);
+    printf("%lu us\n", dt);
+
+    return dt;
+}
+
