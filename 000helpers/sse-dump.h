64d8029e5e60b86080de2e39f86c38ba0ea870a4
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-04-10 19:35:33 +0200

    new SSE dumpers

--- 000helpers/sse-dump.h
+++ 000helpers/sse-dump.h
@@ -25,6 +25,17 @@ void dump_epi16(__m128i v) {
     printf(" ]\n");
 }
 
+void dump_epi32(__m128i v) {
+    int32_t tmp[4];
+
+    _mm_storeu_si128((__m128i*)tmp, v);
+    printf("[ %6d ", tmp[0]);
+    for (int i=1; i < 4; i++)
+        printf("| %12d ", tmp[i]);
+
+    printf(" ]\n");
+}
+
 void dump_epu8_hex(__m128i v) {
     uint8_t tmp[16];
 
@@ -32,6 +43,17 @@ void dump_epu8_hex(__m128i v) {
     printf("[ %02x", tmp[0]);
     for (int i=1; i < 16; i++)
         printf(" | %02x", tmp[i]);
+
+    printf(" ]\n");
+}
+
+void dump_epu32_hex(__m128i v) {
+    uint32_t tmp[4];
+
+    _mm_storeu_si128((__m128i*)tmp, v);
+    printf("[ %02x", tmp[0]);
+    for (int i=1; i < 4; i++)
+        printf(" | %08x", tmp[i]);
 
     printf(" ]\n");
 }
