7f7e18f0f8538bcc100d139b8f2941192f06d59c
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-04-10 21:20:34 +0200

    Measure time in a loop

--- 000helpers/time_utils.h
+++ 000helpers/time_utils.h
@@ -24,3 +24,20 @@ Clock::time_point::rep measure_time(const char* info, FUN fun) {
     return dt;
 }
 
+template <typename FUN>
+Clock::time_point::rep measure_total_time(const char* info, FUN fun, int repeat_count) {
+
+    printf("%s", info);
+    fflush(stdout);
+    const auto t1 = Clock::now();
+    int k = repeat_count;
+    while (k--) {
+        volatile auto tmp = fun();
+    }
+    const auto t2 = Clock::now();
+
+    const auto dt = elapsed(t1, t2);
+    printf("%lu us\n", dt);
+
+    return dt;
+}
