ecb495ff4633320123e3da57493017852cdbf14c
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-04-17 20:30:55 +0200

    time_utils: new variant of measure_time template

--- 000helpers/time_utils.h
+++ 000helpers/time_utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <chrono>
+#include <cstdio>
 
 using Clock = std::chrono::high_resolution_clock;
 
@@ -10,15 +11,21 @@ Clock::time_point::rep elapsed(const Clock::time_point& t1, const Clock::time_po
 }
 
 template <typename FUN>
-Clock::time_point::rep measure_time(const char* info, FUN fun) {
+Clock::time_point::rep measure_time(FUN fun) {
 
-    printf("%s", info);
-    fflush(stdout);
     const auto t1 = Clock::now();
     fun();
     const auto t2 = Clock::now();
 
-    const auto dt = elapsed(t1, t2);
+    return elapsed(t1, t2);
+}
+
+template <typename FUN>
+Clock::time_point::rep measure_time(const char* info, FUN fun) {
+
+    printf("%s", info);
+    fflush(stdout);
+    const auto dt = measure_time(fun);
     printf("%lu us\n", dt);
 
     return dt;
