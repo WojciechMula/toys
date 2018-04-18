e4e8c5927f6f29894fad48c3fbd2b21df578cba3
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-04-18 20:29:47 +0200

    Quiet version of measure_total_time

--- 000helpers/time_utils.h
+++ 000helpers/time_utils.h
@@ -32,10 +32,8 @@ Clock::time_point::rep measure_time(const char* info, FUN fun) {
 }
 
 template <typename FUN>
-Clock::time_point::rep measure_total_time(const char* info, FUN fun, int repeat_count) {
+Clock::time_point::rep measure_total_time(FUN fun, int repeat_count) {
 
-    printf("%s", info);
-    fflush(stdout);
     const auto t1 = Clock::now();
     int k = repeat_count;
     while (k--) {
@@ -43,7 +41,15 @@ Clock::time_point::rep measure_total_time(const char* info, FUN fun, int repeat_
     }
     const auto t2 = Clock::now();
 
-    const auto dt = elapsed(t1, t2);
+    return elapsed(t1, t2);
+}
+
+template <typename FUN>
+Clock::time_point::rep measure_total_time(const char* info, FUN fun, int repeat_count) {
+
+    printf("%s", info);
+    fflush(stdout);
+    const auto dt = measure_total_time(fun, repeat_count);
     printf("%lu us\n", dt);
 
     return dt;
