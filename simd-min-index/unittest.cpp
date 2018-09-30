6f9513ffcc8369a16cfc13a4fcaf747ba588bf69
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-09-30 19:04:03 +0200

    New testcase

--- simd-min-index/unittest.cpp
+++ simd-min-index/unittest.cpp
@@ -31,6 +31,16 @@ public:
 #ifdef HAVE_AVX512
             test_case2("AVX512F",       min_index_avx512f);
 #endif
+
+            test_case3("scalar",        min_index_scalar);
+            test_case3("SSE",           min_index_sse);
+            test_case3("SSE unrolled",  min_index_sse_unrolled);
+#ifdef HAVE_AVX2
+            test_case3("AVX2",          min_index_avx2);
+#endif
+#ifdef HAVE_AVX512
+            test_case3("AVX512F",       min_index_avx512f);
+#endif
         } catch (TestFailed&) {
             return false;
         }
@@ -74,6 +84,24 @@ private:
                     throw TestFailed();
                 }
             }
+        }
+
+        print_ansi("OK\n", ANSI_GREEN);
+    }
+
+    template <typename FUN>
+    void test_case3(const char* name, FUN f) {
+
+        printf("Testing %15s [case 3]... ", name); fflush(stdout);
+
+        size_t result;
+        fill(-1);
+        const size_t expected = 0;
+        f(array, size, &result);
+        if (result != expected) {
+            print_ansi("failed\n", ANSI_RED);
+            printf("expected %lu, result %lu\n", expected, result);
+            throw TestFailed();
         }
 
         print_ansi("OK\n", ANSI_GREEN);
