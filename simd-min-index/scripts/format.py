cb4e22836965432d4e0702537c38ab0aa0bdbc65
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-10-03 19:50:37 +0200

    Fix format utility

--- simd-min-index/scripts/format.py
+++ simd-min-index/scripts/format.py
@@ -3,6 +3,9 @@ from collections import OrderedDict
 from table import Table
 from utils import unicode_bar
 import sys
+import codecs
+
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
 class Measurement:
     __slots__ = ['best', 'avg', 'speedup']
@@ -42,24 +45,38 @@ def load(file):
 
     return result
 
-def render(data):
 
-    table = Table()
-    table.add_header(["procedure", "best",     "avg.",      "speedup", ""])
-    table.add_header(["",          "[cycles]", "[cycles].", "", ""])
+def update_speedup(data):
+
+    global_max_speedup = None
 
     for size, measurements in data.iteritems():
-        table.add_row([("size %d items" % size, 5)])
 
         base_best_time = measurements['scalar'].best
         for name, measurement in measurements.iteritems():
             measurement.speedup = base_best_time/measurement.best
 
         max_speedup = max(m.speedup for m in measurements.itervalues())
+        if global_max_speedup is None:
+            global_max_speedup = max_speedup
+        else:
+            global_max_speedup = max(global_max_speedup, max_speedup)
+
+    return global_max_speedup
+
+
+def render(data, global_max_speedup):
+
+    table = Table()
+    table.add_header(["procedure", "best",     "avg.",      "speedup", ""])
+    table.add_header(["",          "[cycles]", "[cycles]",  "", ""])
+
+    for size, measurements in data.iteritems():
+        table.add_row([("size %d items" % size, 5)])
 
         for name, measurement in measurements.iteritems():
 
-            barlen = 50 * measurement.speedup/max_speedup
+            barlen = 50 * measurement.speedup/global_max_speedup
 
             table.add_row([
                 name,
@@ -75,7 +92,8 @@ def render(data):
 def main():
     with open(sys.argv[1]) as f:
         data = load(f)
-        render(data)
+        global_max_speedup = update_speedup(data)
+        render(data, global_max_speedup)
 
 
 if __name__ == '__main__':
