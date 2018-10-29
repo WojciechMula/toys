e03ceb338864f5d524546cc0c33a5ebc4f34549d
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-10-29 21:03:03 +0100

    Get rid of static const

--- avx512-jpeg-zizag/8bit-array/avx512bw_permute16.cpp
+++ avx512-jpeg-zizag/8bit-array/avx512bw_permute16.cpp
@@ -10,7 +10,7 @@ void jpeg_zigzag_avx512bw_permute16(const uint8_t* in, uint8_t* out) {
     //   [ 6, 7][12,13][14,15][20,21][26,27][28,29][32,33][34,35] -- lane 1: missing: 19, 34, 40, 41, 48
     //   [22,23][30,31][36,37][42,43][44,45][48,49][50,51][56,57] -- lane 2: missing: 15, 29, 35
     //   [38,39][46,47][52,53][54,55][58,59][60,61][62,63][62,62] -- lane 3: missing: 31, 45
-    static const int16_t crosslane_shuffle1[32] __attribute__((aligned(64))) = {
+    const int16_t crosslane_shuffle1[32] __attribute__((aligned(64))) = {
          0,  1,  2,  4,  5,  8,  9, 12,
          3,  6,  7, 10, 13, 14, 16, 17,
         11, 15, 18, 21, 22, 24, 25, 28,
@@ -20,7 +20,7 @@ void jpeg_zigzag_avx512bw_permute16(const uint8_t* in, uint8_t* out) {
     const __m512i shuf1 = _mm512_permutexvar_epi16(_mm512_load_si512((__m512i*)crosslane_shuffle1), v);
 
     // in-lane 8-bit shuffle #1
-    static const int8_t inlane_shuffle1[64] __attribute__((aligned(64))) = {
+    const int8_t inlane_shuffle1[64] __attribute__((aligned(64))) = {
         /* lane 0 */  0,  1,  6, 10,  7,  2,  3,  8, 11, 14, -1, 15, 12,  9,  4,  5,
         /* lane 1 */  2, -1,  8, 13, -1, -1, -1, 14,  9,  6,  3,  0,  1,  4,  7, 10,
         /* lane 2 */ -1,  6, 11, 14, 15, 12,  7,  4, -1,  0, -1,  1,  2,  5,  8, 13,
@@ -34,7 +34,7 @@ void jpeg_zigzag_avx512bw_permute16(const uint8_t* in, uint8_t* out) {
     //   [18,19][34,35][40,41][48,49][48,49][48,49][48,49][48,49] -- lane 1: missing: 19, 34, 40, 41, 48
     //   [14,15][28,29][34,35][34,35][34,35][34,35][34,35][34,35] -- lane 2: missing: 15, 29, 35
     //   [30,31][44,45][44,45][44,45][44,45][44,45][44,45][44,45] -- lane 2: missing: 31, 45
-    static const int16_t crosslane_shuffle2[32] __attribute__((aligned(64))) = {
+    const int16_t crosslane_shuffle2[32] __attribute__((aligned(64))) = {
         /* lane 0*/ 16, 16, 16, 16, 16, 16, 16, 16,
         /* lane 1*/  9, 17, 20, 24, 24, 24, 24, 24,
         /* lane 2*/  7, 14, 17, 17, 17, 17, 17, 17,
@@ -43,7 +43,7 @@ void jpeg_zigzag_avx512bw_permute16(const uint8_t* in, uint8_t* out) {
     const __m512i shuf2 = _mm512_permutexvar_epi16(_mm512_load_si512((__m512i*)crosslane_shuffle2), v);
 
     // in-lane 8-bit shuffle #2
-    static const int8_t inlane_shuffle2[64] __attribute__((aligned(64))) = {
+    const int8_t inlane_shuffle2[64] __attribute__((aligned(64))) = {
         /* lane 0 */ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  0, -1, -1, -1, -1, -1,
         /* lane 1 */ -1,  1, -1, -1,  4,  6,  5,  2, -1, -1, -1, -1, -1, -1, -1, -1,
         /* lane 2 */  5, -1, -1, -1, -1, -1, -1, -1,  3, -1,  1, -1, -1, -1, -1, -1,
