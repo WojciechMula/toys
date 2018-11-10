bf508727224b941e666b45bf1a9bd17c5488ff0e
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2018-11-10 19:38:11 +0100

    make AVX512 dump narrower

--- 000helpers/avx512-dump.h
+++ 000helpers/avx512-dump.h
@@ -18,11 +18,11 @@ void dump_epu8_hex(__m512i v) {
     uint8_t tmp[64];
 
     _mm512_storeu_si512((__m512i*)tmp, v);
-    printf("[ %02x", tmp[0]);
+    printf("[%02x", tmp[0]);
     for (int i=1; i < 64; i++)
-        printf(" | %02x", tmp[i]);
+        printf(" %02x", tmp[i]);
 
-    printf(" ]\n");
+    printf("]\n");
 }
 
 void dump_epu8_ascii(__m512i v) {
