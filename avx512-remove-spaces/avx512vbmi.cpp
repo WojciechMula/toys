79ac1e97d9d3331274d923ebed8e6981866bc98f
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2019-01-05 19:20:56 +0100

    change signature: return past-end pointer

--- avx512-remove-spaces/avx512vbmi.cpp
+++ avx512-remove-spaces/avx512vbmi.cpp
@@ -2,20 +2,11 @@
 #include <cstdint>
 #include <cassert>
 
-void dump(const char* s, uint64_t x) {
-    printf("%s = ", s);
-    while (x) {
-        putchar((x & 1) ? '1' : '0');
-        x >>= 1;
-    }
-
-    putchar('\n');
-}
-
-void remove_spaces__avx512vbmi(const char* src, char* dst, size_t n) {
+char* remove_spaces__avx512vbmi(const char* src, char* dst, size_t n) {
 
     assert(n % 64 == 0);
 
+    // values 0..63
     const __m512i no_gaps_indices = _mm512_setr_epi32(
         0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c,
         0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c,
@@ -23,11 +14,11 @@ void remove_spaces__avx512vbmi(const char* src, char* dst, size_t n) {
         0x33323130, 0x37363534, 0x3b3a3938, 0x3f3e3d3c
     );
 
-    const __m512i ones = _mm512_set1_epi8(1);
+    const __m512i ones   = _mm512_set1_epi8(1);
     const __m512i spaces = _mm512_set1_epi8(' ');
 
-    size_t len = 64;
-    for (size_t j=0, i=0; i < n; i += 64) {
+    size_t len;
+    for (size_t i=0; i < n; i += 64) {
         
         const __m512i input  = _mm512_loadu_si512((const __m512i*)(src + i));
         __m512i output;
@@ -41,10 +32,10 @@ void remove_spaces__avx512vbmi(const char* src, char* dst, size_t n) {
             uint64_t first;
             uint64_t prev;
 
-            first = (mask & -mask);
+            first = (mask & -mask); // isolate the first bit set
             prev  = first;
-            mask ^= first;
-            mask >>= 1;
+            mask ^= first;          // reset it in the mask
+            mask >>= 1;             // and shift by 1
 
             while (mask) {
                 const uint64_t curr = (mask & -mask);
@@ -52,10 +43,11 @@ void remove_spaces__avx512vbmi(const char* src, char* dst, size_t n) {
                 mask >>= 1;
                 
                 if (prev == curr) {
-                    // bit span continues
+                    // run continues
                     increment = _mm512_add_epi8(increment, ones);
                     prev = curr;
                 } else {
+                    // new run
                     indices = _mm512_mask_add_epi8(indices, ~(first - 1), indices, increment);
 
                     first = curr;
@@ -72,8 +64,10 @@ void remove_spaces__avx512vbmi(const char* src, char* dst, size_t n) {
             len = 64;
         }
 
-        _mm512_storeu_si512((__m512i*)(dst + j), output);
-        j += len;
+        _mm512_storeu_si512((__m512i*)(dst), output);
+        dst += len;
     }
+
+    return dst;
 }
 
--- avx512-remove-spaces/scalar.cpp
+++ avx512-remove-spaces/scalar.cpp
@@ -1,6 +1,8 @@
-void remove_spaces__scalar(const char* src, char* dst, size_t n) {
+char* remove_spaces__scalar(const char* src, char* dst, size_t n) {
     for (size_t i=0; i < n; i++) {
         if (src[i] != ' ')
             *dst++ = src[i];
     }
+
+    return dst;
 }
