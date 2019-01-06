8eed3ba8ffb067eef95c71d8d57b10fe7e965c18
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2019-01-06 18:04:17 +0100

    Update benchmark output parser

--- avx512-remove-spaces/scripts/parse.py
+++ avx512-remove-spaces/scripts/parse.py
@@ -26,28 +26,40 @@ class Measurements(object):
 def load(file):
     data = {}
     for c in xrange(0, 64 + 1):
-        data[c] = (Measurements(), Measurements())
+        data[c] = (Measurements(),
+                   Measurements(),
+                   Measurements())
 
     cardinality = None
     for line in file:
         if line.startswith('rdtsc_overhead'):
             continue
 
-        F = line.replace('=', ' ').replace(',', ' ').split()
-        if F[0] == 'cardinality':
+
+        F = line.split(':')
+
+        if len(F) == 1:
+            F = line.replace('=', ' ').replace(',', ' ').split()
+            assert F[0] == 'cardinality'
             cardinality = int(F[1])
             continue
 
         assert cardinality is not None
-        if F[0] == 'scalar':
+        name = F[0].strip()
+        F = F[1].split()
+        if name == 'scalar':
             meas = data[cardinality][0]
-        elif F[0] == 'AVX512VBMI':
+        elif name == 'AVX512VBMI':
             meas = data[cardinality][1]
+        elif name == 'AVX512VBMI (Travis)':
+            meas = data[cardinality][2]
+        elif name == 'AVX512VBMI (Zach)':
+            meas = data[cardinality][2]
         else:
             assert False
 
-        best = float(F[2])
-        avg  = float(F[5])
+        best = float(F[0])
+        avg  = float(F[3])
 
         meas.set_best(best)
         meas.add_value(avg)
