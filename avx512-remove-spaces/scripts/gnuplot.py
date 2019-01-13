7c2419faf603f6dff0b7e9c97b3192c2e566a3a1
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2019-01-13 16:23:20 +0100

    Update plots

--- avx512-remove-spaces/scripts/gnuplot.py
+++ avx512-remove-spaces/scripts/gnuplot.py
@@ -26,8 +26,8 @@ def gnuplot_data(f, data):
         f.write('%d' % cardinality)
         for k in sorted(measurements):
             meas = measurements[k]
-            cycles = meas.best
-            f.write(' %0.3f' % cycles)
+            for cycles in [meas.get_avg(), min(meas.values), max(meas.values), meas.best]:
+                f.write(' %0.3f' % cycles)
 
         f.write('\n')
 
@@ -38,11 +38,22 @@ def gnuplot_generate_plots(keys, data_path):
         title = key
         param = {
             'data'   : data_path,
-            'column' : index + 2,
+            'column' : 4*index + 5,
+            'title'  : title + " (best)",
+        }
+
+        #result.append(GNUPLOT_BEST_PLOT % param)
+
+        param = {
+            'data'   : data_path,
+            'shift'  : index,
+            'avg'    : 4*index + 2,
+            'min'    : 4*index + 3,
+            'max'    : 4*index + 4,
             'title'  : title,
         }
 
-        result.append(GNUPLOT_PLOT % param)
+        result.append(GNUPLOT_AVG_PLOT % param)
 
     return result
 
@@ -58,7 +69,8 @@ def gnuplot_script(file, keys, data_path, png_path, cpu_name):
     file.write(GNUPLOT % param)
 
 
-GNUPLOT_PLOT='"%(data)s" using 1:%(column)d with lines title "%(title)s" noenhanced'
+GNUPLOT_BEST_PLOT='"%(data)s" using 1:%(column)d with lines title "%(title)s" noenhanced'
+GNUPLOT_AVG_PLOT='"%(data)s" using ($1+0.%(shift)d):%(avg)d:%(min)d:%(max)d with yerrorbars title "%(title)s" noenhanced'
 
 GNUPLOT="""
 set terminal png size 800,600
