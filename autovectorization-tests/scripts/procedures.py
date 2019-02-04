6d87c08f2eedeb9e8b2e92bfce18d8af6933129e
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2019-02-04 21:19:21 +0100

    Add helper script
    Building table manually is a tedious and error-prone job

---
+++ autovectorization-tests/scripts/procedures.py
@@ -0,0 +1,156 @@
+import sys
+
+PROCEDURES = {
+    'accumulate_custom.cpp' : {
+        'title'             : "accumulate --- default",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/accumulate',
+        'procedures'        : ["accumulate_custom_epi8", "accumulate_custom_epi32"],
+    },
+
+    'accumulate_default.cpp' : {
+        'title'             : "accumulate --- custom",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/accumulate',
+        'procedures'        : ["accumulate_epi8", "accumulate_epi32"],
+    },
+
+    'all_of.cpp'            : {
+        'title'             : "all_of",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/all_any_none_of',
+        'procedures'        : ["all_of_epi8", "all_of_epi32"],
+    },
+
+    'any_of.cpp'            : {
+        'title'             : "any_of",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/all_any_none_of',
+        'procedures'        : ["any_of_epi8", "any_of_epi32"],
+    },
+
+    'copy.cpp'              : {
+        'title'             : "copy",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/copy',
+        'procedures'        : ["copy_epi8", "copy_epi32"],
+    },
+
+    'copy_if.cpp'           : {
+        'title'             : "copy_if",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/copy',
+        'procedures'        : ["copy_if_epi8", "copy_if_epi32"],
+    },
+
+    'count'                 : {
+        'title'             : "count",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/count',
+        'procedures'        : ["count_epi8", "count_epi32"],
+    },
+
+    'count_if.cpp'          : {
+        'title'             : "count_if",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/count',
+        'procedures'        : ["count_if_epi8", "count_if_epi32"],
+    },
+
+    'fill.cpp'              : {
+        'title'             : "fill",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/fill',
+        'procedures'        : ["fill_epi8", "fill_epi32"],
+    },
+
+    'find.cpp'              : {
+        'title'             : "find",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/find',
+        'procedures'        : ["find_epi8", "find_epi32"],
+    },
+
+    'find_if.cpp'           : {
+        'title'             : "find_if",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/find',
+        'procedures'        : ["find_if_epi8", "find_if_epi32"],
+    },
+
+    'is_sorted.cpp'         : {
+        'title'             : "is_sorted",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/is_sorted',
+        'procedures'        : ["is_sorted_epi8", "is_sorted_epi32"],
+    },
+
+    'none_of.cpp'           : {
+        'title'             : "none_of",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/all_any_none_of',
+        'procedures'        : ["none_of_epi8", "none_of_epi32"],
+    },
+
+    'remove.cpp'            : {
+        'title'             : "remove",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/remove',
+        'procedures'        : ["remove_epi8", "remove_epi32"],
+    },
+
+    'remove_if.cpp'         : {
+        'title'             : "remove_if",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/remove',
+        'procedures'        : ["remove_if_epi8", "remove_if_epi32"],
+    },
+
+    'replace.cpp'           : {
+        'title'             : "replace",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/replace',
+        'procedures'        : ["replace_epi8", "replace_epi32"],
+    },
+
+    'replace_if.cpp'        : {
+        'title'             : "replace_if",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/replace',
+        'procedures'        : ["replace_if_epi8", "replace_if_epi32"],
+    },
+
+    'reverse.cpp'           : {
+        'title'             : "reverse",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/reverse',
+        'procedures'        : ["reverse_epi8", "reverse_epi32"],
+    },
+
+    'transform_abs.cpp'     : {
+        'title'             : "transform --- abs",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/transform',
+        'procedures'        : ["transform_abs_epi8", "transform_abs_epi32"],
+    },
+
+    'transform_inc.cpp'     : {
+        'title'             : "transform --- increment",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/transform',
+        'procedures'        : ["transform_inc_epi8", "transform_inc_epi32"],
+    },
+
+    'transform_neg.cpp'    : {
+        'title'             : "transform --- negation",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/transform',
+        'procedures'        : ["transform_neg_epi8", "transform_neg_epi32"],
+    },
+
+    'unique.cpp'            : {
+        'title'             : "unique",
+        'link'              : 'https://en.cppreference.com/w/cpp/algorithm/unique',
+        'procedures'        : ["unique_epi8", "unique_epi32"],
+    },
+}
+
+
+def print_checklist(file):
+    procedures = []
+    for item in PROCEDURES.values():
+        for procedure in item['procedures']:
+            procedures.append(procedure)
+
+    def writeln(s):
+        file.write(s)
+        file.write('\n')
+   
+    writeln('compiler: ')
+    writeln('cmdline: ')
+    writeln('')
+    for procedure in sorted(procedures):
+        writeln('%s: ??' % procedure)
+   
+
+if __name__ == '__main__':
+    print_checklist(sys.stdout)
---
+++ autovectorization-tests/scripts/summary.py
@@ -0,0 +1,179 @@
+from os.path import join
+from procedures import PROCEDURES
+from table import Table
+
+GITHUB_LINK = 'https://github.com/WojciechMula/toys/blob/master/autovectorization-tests/%s'
+
+COMPILERS = {
+    'GCC' : {
+                'avx2'   : 'gcc9-avx2.txt',
+                'avx512' : 'gcc9-avx512.txt',
+    },
+    'Clang' : {
+                'avx2'   : 'clang9-avx2.txt',
+                'avx512' : 'clang9-avx512.txt',
+    },
+    'ICC' : {
+                'avx2'   : 'icc19-avx2.txt',
+                'avx512' : 'icc19-avx512.txt',
+    },
+    'MSVC' : {
+                'avx2'   : 'msvc19-avx2.txt',
+                'avx512' : 'msvc19-avx512.txt',
+    },
+}
+
+COMPILERS_ORDER = ['GCC', 'Clang', 'ICC', 'MSVC']
+
+
+def load_results(file):
+    compiler  = None
+    cmdline   = None
+    status    = {}
+    for line in file:
+        line = line.strip()
+        if not line:
+            continue
+
+        key, value = line.split(':', 1)
+
+        key   = key.strip()
+        value = value.strip()
+
+        if key == 'compiler':
+            compiler = value
+        elif key == 'cmdline':
+            cmdline = value
+        else:
+            assert key not in status
+            status[key] = value
+
+    return (compiler, cmdline, status)
+
+
+def load_all_results(rootdir):
+    for compiler_name in COMPILERS_ORDER:
+        compiler = COMPILERS[compiler_name]
+        for arch_name in compiler:
+            filename = compiler[arch_name]
+            path = join(rootdir, filename)
+            with open(path, 'rt') as f:
+                data = load_results(f)
+                compiler[arch_name] = data
+
+
+class Formatter(object):
+    def __init__(self):
+        load_all_results('results')
+        self.footnotes = {}
+        self.links = []
+
+
+    def run(self):
+        table = Table()
+
+        header_row1 = ["algorithm", "procedure"]
+        header_row2 = ["", ""]
+        for compiler_name in COMPILERS_ORDER:
+            header_row1.append((compiler_name, 2))
+            header_row2.append("AVX2")
+            header_row2.append("AVX512")
+
+        table.add_header(header_row1)
+        table.add_header(header_row2)
+
+        for source_name in sorted(PROCEDURES):
+            item = PROCEDURES[source_name]
+            for index, procedure_name in enumerate(item['procedures']):
+                row = []
+                if index == 0:
+                    row.append(self.format_title(source_name, item))
+                else:
+                    row.append("")
+
+                if index == 0:
+                    row.append(self.format_procedure(source_name, procedure_name))
+                else:
+                    row.append(procedure_name)
+
+                for compiler_name in COMPILERS_ORDER:
+                    avx2 = COMPILERS[compiler_name]['avx2'][2]
+                    row.append(self.format_status(avx2[procedure_name]))
+                    avx512 = COMPILERS[compiler_name]['avx512'][2]
+                    row.append(self.format_status(avx512[procedure_name]))
+
+                table.add_row(row)
+
+        self.table = table
+
+        self.print_result()
+
+
+    def print_result(self):
+        print self.table
+
+        if len(self.links):
+            print
+            for link in self.links:
+                print "__ %s" % link
+
+        if len(self.footnotes) > 0:
+            tmp = [(fn_idx, text) for text, fn_idx in self.footnotes.items()]
+            tmp.sort()
+
+            print
+            for fn_idx, text in tmp:
+                print ".. [%d] %s" % (fn_idx, text)
+
+
+    def register_link(self, link):
+        self.links.append(link)
+        return link
+
+
+    def format_title(self, source_name, item):
+        link = None
+        if 'link' in item:
+            link = self.register_link(item['link'])
+
+        if item['title'] == '':
+            title = source_name
+        else:
+            title = item['title']
+       
+        if link is not None:
+            return "`%s`__" % title
+        else:
+            return title
+
+
+    def format_procedure(self, source_name, procedure_name):
+        link = GITHUB_LINK % source_name
+        self.register_link(link)
+        return '`%s`__' % procedure_name
+
+
+    def format_status(self, text):
+        if text == 'yes':
+            return '**%s**' % text
+        elif text == 'no':
+            return text
+        else:
+            status, note = text.split(' ', 1)
+            status = status.strip()
+            note   = note.strip()
+
+            assert status == 'no'
+            assert note[0] == '['
+            assert note[-1] == ']'
+
+            note = note[1:-1]
+
+            if note not in self.footnotes:
+                self.footnotes[note] = len(self.footnotes) + 1
+
+            fn_idx = self.footnotes[note]
+            return '%s [%d]_' % (status, fn_idx)
+
+fmt = Formatter()
+fmt.run()
