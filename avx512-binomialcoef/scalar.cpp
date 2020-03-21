6a34bdb8885c37e532ab0059cfad2d650545cd1e
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2020-03-21 15:20:52 +0100

    Simplify scalar version

--- avx512-binomialcoef/scalar.cpp
+++ avx512-binomialcoef/scalar.cpp
@@ -1,20 +1,13 @@
 #include <cstdint>
 #include <cassert>
 
-uint64_t binom_aux(uint64_t n, uint64_t p);
-
 uint64_t binom_scalar(uint64_t n, uint64_t k) {
     assert(n > 1);
     assert(k > 1);
     assert(k < n);
 
-    if (k <= (n - k))
-        return binom_aux(n, k);
-    else
-        return binom_aux(n, n - k);
-}
-
-uint64_t binom_aux(uint64_t n, uint64_t p) {
+    const uint64_t p = std::min(k, n - k);
+    
     uint64_t num = 1;
     uint64_t denom = 1;
     for (uint64_t i=0; i < p; i++) {
