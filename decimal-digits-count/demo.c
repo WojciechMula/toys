1735660eb4c263977c1103ca8027ba27ace780ff
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2021-01-24 13:22:14 +0100

    Simplify demo utility

--- decimal-digits-count/demo.c
+++ decimal-digits-count/demo.c
@@ -14,27 +14,28 @@
 int main(int argc, char* argv[]) {
 	for (int i=1; i < argc; i++) {
 		const int x = atoi(argv[i]);
-		if (x >= 0) {
-			const int ref = decimal_digits_naive(x);
-            printf("%15d: digits %2d (correct)", x, ref);
+		if (x < 0) {
+			printf("%15d: negative numbers not supported\n", x);
+            continue;
+        }
+            
+        const int ref = decimal_digits_naive(x);
+        printf("%15d: digits %2d (correct)", x, ref);
 
-			const int sse = decimal_digits_sse(x);
-			printf(", %2d (SSE proc)", sse);
-            if (sse != ref)
-                printf(" FAILED");
+        const int sse = decimal_digits_sse(x);
+        printf(", %2d (SSE proc)", sse);
+        if (sse != ref)
+            printf(" FAILED");
 
 #ifdef AVX512
-			const int avx512 = decimal_digits_avx512(x);
-		
-			printf(", %2d (AVX512 proc)", avx512);
-            if (avx512 != ref)
-                printf(" FAILED");
+        const int avx512 = decimal_digits_avx512(x);
+    
+        printf(", %2d (AVX512 proc)", avx512);
+        if (avx512 != ref)
+            printf(" FAILED");
 
 #endif
-            putchar('\n');
-		} else {
-			printf("%15d: negative numbers not supported\n", x);
-		}
+        putchar('\n');
 	}
 
 	return EXIT_SUCCESS;
