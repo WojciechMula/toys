e2d500aa53591f9c86523fb39a225e7060bcb870
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2021-01-30 11:24:51 +0100

    Lookup using perfect hashing (gperf)

---
+++ verb-parse/verb-parse-perfhash.in.cpp
@@ -0,0 +1,167 @@
+/* C++ code produced by gperf version 3.1 */
+/* Command-line: gperf --language=C++ --enum --readonly-tables --compare-strncmp --class-name=perfect_hash --lookup-function-name=string_to_verb --output-file=verb-parse-perfhash.in.cpp http_verbs.txt  */
+/* Computed positions: -k'1,3-4' */
+
+#if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
+      && ('%' == 37) && ('&' == 38) && ('\'' == 39) && ('(' == 40) \
+      && (')' == 41) && ('*' == 42) && ('+' == 43) && (',' == 44) \
+      && ('-' == 45) && ('.' == 46) && ('/' == 47) && ('0' == 48) \
+      && ('1' == 49) && ('2' == 50) && ('3' == 51) && ('4' == 52) \
+      && ('5' == 53) && ('6' == 54) && ('7' == 55) && ('8' == 56) \
+      && ('9' == 57) && (':' == 58) && (';' == 59) && ('<' == 60) \
+      && ('=' == 61) && ('>' == 62) && ('?' == 63) && ('A' == 65) \
+      && ('B' == 66) && ('C' == 67) && ('D' == 68) && ('E' == 69) \
+      && ('F' == 70) && ('G' == 71) && ('H' == 72) && ('I' == 73) \
+      && ('J' == 74) && ('K' == 75) && ('L' == 76) && ('M' == 77) \
+      && ('N' == 78) && ('O' == 79) && ('P' == 80) && ('Q' == 81) \
+      && ('R' == 82) && ('S' == 83) && ('T' == 84) && ('U' == 85) \
+      && ('V' == 86) && ('W' == 87) && ('X' == 88) && ('Y' == 89) \
+      && ('Z' == 90) && ('[' == 91) && ('\\' == 92) && (']' == 93) \
+      && ('^' == 94) && ('_' == 95) && ('a' == 97) && ('b' == 98) \
+      && ('c' == 99) && ('d' == 100) && ('e' == 101) && ('f' == 102) \
+      && ('g' == 103) && ('h' == 104) && ('i' == 105) && ('j' == 106) \
+      && ('k' == 107) && ('l' == 108) && ('m' == 109) && ('n' == 110) \
+      && ('o' == 111) && ('p' == 112) && ('q' == 113) && ('r' == 114) \
+      && ('s' == 115) && ('t' == 116) && ('u' == 117) && ('v' == 118) \
+      && ('w' == 119) && ('x' == 120) && ('y' == 121) && ('z' == 122) \
+      && ('{' == 123) && ('|' == 124) && ('}' == 125) && ('~' == 126))
+/* The character set is not based on ISO-646.  */
+#error "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gperf@gnu.org>."
+#endif
+
+/* maximum key range = 67, duplicates = 0 */
+
+class perfect_hash
+{
+private:
+  static inline unsigned int hash (const char *str, size_t len);
+public:
+  static const char *string_to_verb (const char *str, size_t len);
+};
+
+inline unsigned int
+perfect_hash::hash (const char *str, size_t len)
+{
+  static const unsigned char asso_values[] =
+    {
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 10,  5, 15,  0,  5,
+      25,  0, 10, 70, 15, 70,  0,  0, 20,  0,
+       5, 25, 20,  0,  0,  0, 40, 70, 70, 70,
+      20, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70, 70, 70, 70,
+      70, 70, 70, 70, 70, 70, 70
+    };
+  unsigned int hval = len;
+
+  switch (hval)
+    {
+      default:
+        hval += asso_values[static_cast<unsigned char>(str[3]+1)];
+      /*FALLTHROUGH*/
+      case 3:
+        hval += asso_values[static_cast<unsigned char>(str[2])];
+      /*FALLTHROUGH*/
+      case 2:
+      case 1:
+        hval += asso_values[static_cast<unsigned char>(str[0])];
+        break;
+    }
+  return hval;
+}
+
+const char *
+perfect_hash::string_to_verb (const char *str, size_t len)
+{
+  enum
+    {
+      TOTAL_KEYWORDS = 33,
+      MIN_WORD_LENGTH = 3,
+      MAX_WORD_LENGTH = 11,
+      MIN_HASH_VALUE = 3,
+      MAX_HASH_VALUE = 69
+    };
+
+  static const char * const wordlist[] =
+    {
+      "", "", "",
+      "GET",
+      "", "", "", "",
+      "PUT",
+      "POST",
+      "PATCH",
+      "UNLOCK",
+      "",
+      "ACL",
+      "SUBSCRIBE",
+      "TRACE",
+      "SEARCH",
+      "", "",
+      "LOCK",
+      "MKACTIVITY",
+      "UNLINK",
+      "OPTIONS",
+      "",
+      "LINK",
+      "MKCOL",
+      "UNBIND",
+      "",
+      "CHECKOUT",
+      "HEAD",
+      "MKCALENDAR",
+      "DELETE",
+      "",
+      "M-SEARCH",
+      "BIND",
+      "MERGE",
+      "REPORT",
+      "",
+      "PROPFIND",
+      "PROPPATCH",
+      "PURGE",
+      "NOTIFY",
+      "CONNECT",
+      "",
+      "COPY",
+      "",
+      "REBIND",
+      "", "", "", "",
+      "UNSUBSCRIBE",
+      "", "", "", "", "", "", "", "", "",
+      "", "", "", "", "", "", "", "",
+      "MOVE"
+    };
+
+  if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
+    {
+      unsigned int key = hash (str, len);
+
+      if (key <= MAX_HASH_VALUE)
+        {
+          const char *s = wordlist[key];
+
+          if (*str == *s && !strncmp (str + 1, s + 1, len - 1) && s[len] == '\0')
+            return s;
+        }
+    }
+  return 0;
+}
