e126fa953918caaedc829940ccded34c2484e125
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2023-02-02 20:57:12 +0100

    better tests & benchmarks

--- powfraction/perf_test.go
+++ powfraction/perf_test.go
@@ -8,38 +8,58 @@ import (
 
 func TestPowFract(t *testing.T) {
 	f := 781.45
-	exponents := []int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 100}
-	for _, exponent := range exponents {
-		powfrac(f, float64(1)/float64(exponent))
+	for _, basefloat := range []float64{0.5, 0.25, 0.125, 0.0625, 0.03125} {
+		img := math.Float64bits(basefloat)
+		for pattern := uint64(0); pattern < (1 << bitCount); pattern++ {
+			tmp := img | (pattern << (52 - bitCount))
+			exponent := math.Float64frombits(tmp)
+			t.Run(fmt.Sprintf("%f", exponent), func(t *testing.T) {
+				got := fmt.Sprintf("%f", powfrac(f, exponent))
+				want := fmt.Sprintf("%f", math.Pow(f, exponent))
+				if got != want {
+					t.Logf("got:  %s", got)
+					t.Logf("want: %s", want)
+					t.Errorf("wrong result")
+				}
+			})
+		}
 	}
 }
 
+const bitCount = 8
+
 func BenchmarkPowStd(b *testing.B) {
 	f := 781.45
-	exponents := []int{2, 3, 4, 8, 16, 32, 64, 128, 256}
-	for _, exponent := range exponents {
-		exponent := exponent
-		b.Run(fmt.Sprintf("1-over-%d", exponent), func(b *testing.B) {
-			e := float64(1) / float64(exponent)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				_ = math.Pow(f, e)
-			}
-		})
+
+	for _, basefloat := range []float64{0.5, 0.25, 0.125, 0.0625, 0.03125} {
+		img := math.Float64bits(basefloat)
+		for pattern := uint64(0); pattern < (1 << bitCount); pattern++ {
+			tmp := img | (pattern << (52 - bitCount))
+			exponent := math.Float64frombits(tmp)
+			b.Run(fmt.Sprintf("%f", exponent), func(b *testing.B) {
+				b.ResetTimer()
+				for i := 0; i < b.N; i++ {
+					_ = math.Pow(f, exponent)
+				}
+			})
+		}
 	}
 }
 
 func BenchmarkPowFract(b *testing.B) {
 	f := 781.45
-	exponents := []int{2, 3, 4, 8, 16, 32, 64, 128, 256}
-	for _, exponent := range exponents {
-		exponent := exponent
-		b.Run(fmt.Sprintf("1-over-%d", exponent), func(b *testing.B) {
-			e := float64(1) / float64(exponent)
-			b.ResetTimer()
-			for i := 0; i < b.N; i++ {
-				_ = powfrac(f, e)
-			}
-		})
+
+	for _, basefloat := range []float64{0.5, 0.25, 0.125, 0.0625, 0.03125} {
+		img := math.Float64bits(basefloat)
+		for pattern := uint64(0); pattern < (1 << bitCount); pattern++ {
+			tmp := img | (pattern << (52 - bitCount))
+			exponent := math.Float64frombits(tmp)
+			b.Run(fmt.Sprintf("%f", exponent), func(b *testing.B) {
+				b.ResetTimer()
+				for i := 0; i < b.N; i++ {
+					_ = powfrac(f, exponent)
+				}
+			})
+		}
 	}
 }
