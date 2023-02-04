f46b7c7b9d3dd6fa31068fb344168f5b07084450
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2023-02-04 23:37:56 +0100

    simpler benchmarks

--- powfraction/perf_test.go
+++ powfraction/perf_test.go
@@ -6,8 +6,11 @@ import (
 	"testing"
 )
 
+const bitCount = 8
+
 func TestPowFract(t *testing.T) {
 	f := 781.45
+
 	for _, basefloat := range []float64{0.5, 0.25, 0.125, 0.0625, 0.03125} {
 		img := math.Float64bits(basefloat)
 		for pattern := uint64(0); pattern < (1 << bitCount); pattern++ {
@@ -26,40 +29,30 @@ func TestPowFract(t *testing.T) {
 	}
 }
 
-const bitCount = 8
-
 func BenchmarkPowStd(b *testing.B) {
 	f := 781.45
 
-	for _, basefloat := range []float64{0.5, 0.25, 0.125, 0.0625, 0.03125} {
-		img := math.Float64bits(basefloat)
-		for pattern := uint64(0); pattern < (1 << bitCount); pattern++ {
-			tmp := img | (pattern << (52 - bitCount))
-			exponent := math.Float64frombits(tmp)
-			b.Run(fmt.Sprintf("%f", exponent), func(b *testing.B) {
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					_ = math.Pow(f, exponent)
-				}
-			})
-		}
+	for i := 1; i < 16; i++ {
+		exponent := float64(1) / float64(uint64(1<<i))
+		b.Run(fmt.Sprintf("2^{-%d}", i), func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_ = math.Pow(f, exponent)
+			}
+		})
 	}
 }
 
 func BenchmarkPowFract(b *testing.B) {
 	f := 781.45
 
-	for _, basefloat := range []float64{0.5, 0.25, 0.125, 0.0625, 0.03125} {
-		img := math.Float64bits(basefloat)
-		for pattern := uint64(0); pattern < (1 << bitCount); pattern++ {
-			tmp := img | (pattern << (52 - bitCount))
-			exponent := math.Float64frombits(tmp)
-			b.Run(fmt.Sprintf("%f", exponent), func(b *testing.B) {
-				b.ResetTimer()
-				for i := 0; i < b.N; i++ {
-					_ = powfrac(f, exponent)
-				}
-			})
-		}
+	for i := 1; i < 16; i++ {
+		exponent := float64(1) / float64(uint64(1<<i))
+		b.Run(fmt.Sprintf("2^{-%d}", i), func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_ = powfrac(f, exponent)
+			}
+		})
 	}
 }
