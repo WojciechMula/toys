5c1d6cc59577b3b5b7f3ddfd67bc3503aee8ec40
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2024-12-18 23:45:06 +0100

    rest: tabs to spaces

--- restructuredtext/myroles.py
+++ restructuredtext/myroles.py
@@ -2,28 +2,28 @@ from roles_aux import wikilink, simplemath, py_getdef
 from docutils.parsers.rst.roles import set_classes, nodes, register_canonical_role
 
 def plwiki_link_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
-	set_classes(options)
-	ref, text = wikilink(text, 'pl')
-	return [nodes.reference(rawtext, nodes.unescape(text), refuri=ref, **options)], []
+    set_classes(options)
+    ref, text = wikilink(text, 'pl')
+    return [nodes.reference(rawtext, nodes.unescape(text), refuri=ref, **options)], []
 
 def enwiki_link_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
-	set_classes(options)
-	ref, text = wikilink(text, 'en')
-	return [nodes.reference(rawtext, nodes.unescape(text), refuri=ref, **options)], []
+    set_classes(options)
+    ref, text = wikilink(text, 'en')
+    return [nodes.reference(rawtext, nodes.unescape(text), refuri=ref, **options)], []
 
 def inlinemath_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
-	set_classes(options)
-	node = nodes.raw(rawtext, simplemath(text), **options)
-	node['classes'] = ['math']
-	node['format']  = 'html'
-	return [node], []
+    set_classes(options)
+    node = nodes.raw(rawtext, simplemath(text), **options)
+    node['classes'] = ['math']
+    node['format']  = 'html'
+    return [node], []
 
 def centermath_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
-	set_classes(options)
-	node = nodes.raw(rawtext, simplemath(text), **options)
-	node['classes'] = ['cmath']
-	node['format']  = 'html'
-	return [node], []
+    set_classes(options)
+    node = nodes.raw(rawtext, simplemath(text), **options)
+    node['classes'] = ['cmath']
+    node['format']  = 'html'
+    return [node], []
 
 
 register_canonical_role('math',   inlinemath_role)
