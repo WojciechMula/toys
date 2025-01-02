e29f6325403c9b574b2ff7896cf5ddc7a8d84638
Wojciech Muła <wojciech_mula@poczta.onet.pl>
2025-01-02 21:24:20 +0100

    u16div: add helper scripts

---
+++ u16div/results/format.py
@@ -0,0 +1,36 @@
+import sys
+import codecs
+
+from benchmark_parser import parse, update_speedup, get_maximum_speedup
+from table import Table
+from utils import unicode_bar
+
+
+def main():
+    with open(sys.argv[1], 'rt') as f:
+        data = parse(f)
+        update_speedup(data, 'scalar')
+        data = data[0]
+
+    table = Table()
+    table.add_header(["procedure", ("time in cycles per byte", 2), ("speed-up", 2)])
+    table.add_header(["", "average", "best", "", ""])
+
+    for procedure in data:
+        m = data[procedure]
+
+        row = [
+            procedure,
+            '%0.3f' % m.avg,
+            '%0.3f' % m.best,
+            '%0.1f' % m.speedup,
+            '``%s``' % unicode_bar(40 * m.speedup / get_maximum_speedup(data))
+        ]
+
+        table.add_row(row)
+
+    print(table)
+
+
+if __name__ == '__main__':
+    main()
